717850145fc4324806c5f74f55b4e4c5f1675205
AntonioSilva <antonio.ifusp@gmail.com>
2019-11-01 14:33:11 -0400

    Implemented Correlator class

--- 1110.5800/STAR_2012_I943192.cc
+++ 1110.5800/STAR_2012_I943192.cc
@@ -1,4 +1,3 @@
-
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 //#include "Rivet/Projections/FinalState.hh"
@@ -9,15 +8,87 @@
 #include <iostream>
 #include <math.h>
 #include <vector> 
+#include "Rivet/Projections/RHICCentrality.hh"
 #define _USE_MATH_DEFINES
-static const int numTrigPtBins = 7;
-static const float pTTrigBins[] = {2.0,2.5,3.0,3.5,4.0,4.5,5.0,6.0};
-static const int numAssocPtBins = 9;
-static const float pTAssocBins[] = {1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,6.0};
-static const int numCentBins = 9;
-static const int centBins[] = {0,5,10,20, 30,40,50,60, 70,80};
+
 using namespace std;
 namespace Rivet {
+    
+    class Correlator {
+  
+  public:
+    
+    /// Constructor
+    Correlator(int index) {
+      _index = index;
+    }
+
+    void SetCollSystemAndEnergy(string s){ _collSystemAndEnergy = s; }
+    void SetCentrality(double cmin, double cmax){ _centrality = make_pair(cmin, cmax); }
+    void SetTriggerRange(double tmin, double tmax){ _triggerRange = make_pair(tmin, tmax); }
+    void SetAssociatedRange(double amin, double amax){ _associatedRange = make_pair(amin, amax); }
+    
+    string GetCollSystemAndEnergy(){ return _collSystemAndEnergy; }
+    pair<double,double> GetCentrality(){ return _centrality; }
+    double GetCentralityMin(){ return _centrality.first; }
+    double GetCentralityMax(){ return _centrality.second; }
+    pair<double,double> GetTriggerRange(){ return _triggerRange; }
+    double GetTriggerRangeMin(){ return _triggerRange.first; }
+    double GetTriggerRangeMax(){ return _triggerRange.second; }
+    pair<double,double> GetAssociatedRange(){ return _associatedRange; }
+    double GetAssociatedRangeMin(){ return _associatedRange.first; }
+    double GetAssociatedRangeMax(){ return _associatedRange.second; }
+    
+    int GetIndex(){ return _index; }
+    
+    bool CheckCollSystemAndEnergy(string s){ return _collSystemAndEnergy.compare(s) == 0 ? true : false; }
+    bool CheckCentrality(double cent){ return (cent>_centrality.first && cent<_centrality.second) ? true : false; }
+    bool CheckTriggerRange(double tpt){ return (tpt>_triggerRange.first && tpt<_triggerRange.second) ? true : false; }
+    bool CheckAssociatedRange(double apt){ return (apt>_associatedRange.first && apt<_associatedRange.second) ? true : false; }
+    bool CheckAssociatedRangeMaxTrigger(double apt, double tpt){ return (apt>_associatedRange.first && apt<tpt) ? true : false; }
+    bool CheckConditions(string s, double cent, double tpt, double apt)
+    {
+        if(!CheckConditions(s, cent, tpt)) return false;
+        if(!CheckAssociatedRange(apt)) return false;
+        
+        return true;
+        
+    }
+    bool CheckConditions(string s, double cent, double tpt)
+    {
+        if(!CheckConditions(s, cent)) return false;
+        if(!CheckTriggerRange(tpt)) return false;
+        
+        return true;
+        
+    }
+    bool CheckConditions(string s, double cent)
+    {
+        if(!CheckCollSystemAndEnergy(s)) return false;
+        if(!CheckCentrality(cent)) return false;
+        
+        return true;
+        
+    }
+    bool CheckConditionsMaxTrigger(string s, double cent, double tpt, double apt)
+    {
+        if(!CheckCollSystemAndEnergy(s)) return false;
+        if(!CheckCentrality(cent)) return false;
+        if(!CheckTriggerRange(tpt)) return false;
+        if(!CheckAssociatedRangeMaxTrigger(apt,tpt)) return false;
+        
+        return true;
+        
+    }
+    
+    int _index;
+    string _collSystemAndEnergy;
+    pair<double,double> _centrality;
+    pair<double,double> _triggerRange;
+    pair<double,double> _associatedRange;
+
+  
+  };
 
   class STAR_2012_I943192 : public Analysis {
   public:
@@ -27,73 +98,96 @@ namespace Rivet {
 
 
     /// name Analysis methods
-    int GetTrigBin(float trigpT){
-      if(trigpT<pTTrigBins[0]){
-	cerr<<"Warning: trigpT "<<trigpT<<" is less than the minimum trigger momentum!"<<endl;
-	return -1;
-      }
-      for(int i=0;i<numTrigPtBins;i++){
-	if(trigpT<pTTrigBins[i+1]) return i;
-      }
-      cerr<<"Warning: trigpT "<<trigpT<<" is greater than the maximum trigger momentum!"<<endl;
-      return -1;
+    bool isSameParticle(const Particle& p1, const Particle& p2)
+    {
+        //if pT, eta and phi are equal, they are the same particle
+        if(p1.pt() != p2.pt()) return false;
+        if(p1.eta() != p2.eta()) return false;
+        if(p1.phi() != p2.phi()) return false;
+    
+        return true;
     }
-    int GetAssocBin(float assocpT){
-      if(assocpT<pTAssocBins[0]){
-	cerr<<"Warning: assocpT "<<assocpT<<" is less than the minimum associated momentum! "<<endl;
-	return -1;
-      }
-      for(int i=0;i<numAssocPtBins;i++){
-	if(assocpT<pTAssocBins[i+1]) return i;
-      }
-      cerr<<"Warning: assocpT "<<assocpT<<" is greater than the maximum associated momentum!"<<endl;
-      return -1;
-    }
-    int GetCentBin(float cent){
-      if(cent<centBins[0]){
-	cerr<<"Warning: cent "<<cent<<" is less than the minimum centrality bin! "<<endl;
-	return -1;
-      }
-      for(int i=0;i<numCentBins;i++){
-	if(cent<centBins[i+1]) return i;
-      }
-      cerr<<"Warning: cent "<<cent<<" is greater than the maximum centrality bin!"<<endl;
-      return -1;
+    
+    bool isSecondary(Particle p)
+    {
+        //return true if is secondary
+        if (( p.hasAncestor(310) || p.hasAncestor(-310)  ||     // K0s
+	           p.hasAncestor(130)  || p.hasAncestor(-130)  ||     // K0l
+	           p.hasAncestor(3322) || p.hasAncestor(-3322) ||     // Xi0
+	           p.hasAncestor(3122) || p.hasAncestor(-3122) ||     // Lambda
+	           p.hasAncestor(3222) || p.hasAncestor(-3222) ||     // Sigma+/-
+	           p.hasAncestor(3312) || p.hasAncestor(-3312) ||     // Xi-/+
+	           p.hasAncestor(3334) || p.hasAncestor(-3334) ))    // Omega-/+
+        return true;
+        else return false;
+        
     }
 
     /// Book histograms and initialise projections before the run
     void init() {
 
-      // Initialise and register projections
-
       // the basic final-state projection: all final-state particles within the given eta acceptance
 
-      const ChargedFinalState cfs(Cuts::abseta < 1.0 && Cuts::pT > 1*GeV);
+      const ChargedFinalState cfs(Cuts::pT > 1*GeV); //Not cutting in eta, so no need to correct for pair acceptance
       declare(cfs, "CFS");
       const ChargedFinalState cfsTrig(Cuts::abseta < 1.0 && Cuts::pT > 2*GeV);
       declare(cfsTrig, "CFSTrig");
-      // FinalState of prompt photons and bare muons and electrons in the event
-      //PromptFinalState photons(Cuts::abspid == PID::PHOTON);
 
       // Declare centrality projection
       //LATER FIX TO USE STAR
-      declareCentrality(ALICE::V0MMultiplicity(), "ALICE_2015_PBPBCentrality", "V0M", "V0M");
+      //declareCentrality(ALICE::V0MMultiplicity(), "ALICE_2015_PBPBCentrality", "V0M", "V0M");
+      declareCentrality(RHICCentrality("STAR"), "RHIC_2019_CentralityCalibration:exp=STAR", "CMULT", "CMULT");
 
-      // Book histograms
-      //efficiency - this was just a plot to demonstrate the method
-      //book(_h["0111"], 1, 1, 1);
-      //correlation functions in Au-Au before and after track merging, demonstrates the method
-      //book(_h["0211"], 2, 1, 1);
-      //book(_h["0212"], 2, 1, 2);
-      //Order of histograms:
-      //Cu+Cu 62.4 GeV 0-60%
-      //Au+Au 62.4 GeV 0-80$
-      //d+Au 200 GeV
-      //Cu+Cu 200 GeV 0-60%
-      //Au+Au 200 GeV 40-80%
-      //Au+Au 200 GeV 0-12%
-      //For Npart histogram, 5th data set is all Au+Au and centrality bin ranges are not relevant
-      //sample correlation functions in delta eta before background subtraction (Fig. 5)
+      //==================================================
+      // Create one correlator for each set of Collisions System / Beam Energy / Centrality Interval / Trigger pT interval / Associated pT interval
+      // The number used in the constructor is the y-axis index of the histograms (d00-x00-y00) of the paper
+      // Ex.: Correlator c1(1); -> is the correlator for histograms _h["0311"], _h["0411"], etc
+      // Ex.: Correlator c2(2); -> is the correlator for histograms _h["0312"], _h["0412"], etc
+       //==================================================
+      
+      Correlator c1(1);
+      c1.SetCollSystemAndEnergy("CuCu62GeV");
+      c1.SetCentrality(0., 60.);
+      c1.SetTriggerRange(3., 6.);
+      c1.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c1);
+      
+      Correlator c2(2);
+      c2.SetCollSystemAndEnergy("AuAu62GeV");
+      c2.SetCentrality(0., 80.);
+      c2.SetTriggerRange(3., 6.);
+      c2.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c2);
+      
+      Correlator c3(3);
+      c3.SetCollSystemAndEnergy("dAu200GeV");
+      c3.SetCentrality(0., 95.);
+      c3.SetTriggerRange(3., 6.);
+      c3.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c3);
+      
+      Correlator c4(4);
+      c4.SetCollSystemAndEnergy("CuCu200GeV");
+      c4.SetCentrality(0., 60.);
+      c4.SetTriggerRange(3., 6.);
+      c4.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c4);
+      
+      Correlator c5(5);
+      c5.SetCollSystemAndEnergy("AuAu200GeV");
+      c5.SetCentrality(40., 80.);
+      c5.SetTriggerRange(3., 6.);
+      c5.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c5);
+      
+      Correlator c6(6);
+      c6.SetCollSystemAndEnergy("AuAu200GeV");
+      c6.SetCentrality(0., 12.);
+      c6.SetTriggerRange(3., 6.);
+      c6.SetAssociatedRange(1.5, 999.);
+      Correlators.push_back(c6);
+      
+      /*
       book(_h["0311"], 3, 1, 1);
       book(_h["0312"], 3, 1, 2);
       book(_h["0313"], 3, 1, 3);
@@ -210,6 +304,18 @@ namespace Rivet {
       //book(_h["3911"], 39, 1, 1);
       //book(_h["4011"], 40, 1, 1);
       //Add declaration of histograms for correlation functions with all correlation functions
+      */
+      
+      for(unsigned int i = 1; i<= Correlators.size(); i++)
+      {
+          book(sow[i],"sow" + to_string(i));
+          book(_h["031" + to_string(i)], 3, 1, i);
+          book(_h["041" + to_string(i)], 4, 1, i);
+      }
+      
+      nEvents.assign(Correlators.size()+1, 0); 
+      nTriggers.assign(Correlators.size()+1, 0); 
+      
     }
 
 
@@ -219,68 +325,119 @@ namespace Rivet {
       const ChargedFinalState& cfs = apply<ChargedFinalState>(event, "CFS");
       const ChargedFinalState& cfsTrig = apply<ChargedFinalState>(event, "CFSTrig");
 
-      // Prepare centrality projection and value
-      const CentralityProjection& centrProj = apply<CentralityProjection>(event, "V0M");
-      double centr = centrProj();
-
-      // Veto event for too large centralities since those are not used in the analysis at all
-      if ((centr < 0.) || (centr > 80.)){
-        vetoEvent;
+      //==================================================
+      // Select the histograms accordingly to the collision system, beam energy and centrality
+      // WARNING: Still not implemented for d-Au
+      //==================================================
+      double nNucleons = 0.;
+      string CollSystem = "Empty";
+      const ParticlePair& beam = beams();
+      if (beam.first.pid() == 1000791970 && beam.second.pid() == 1000791970)
+      {
+          CollSystem = "AuAu";
+          nNucleons = 197.;
       }
-      cout<<"Hi"<<endl;
-      int mycb = GetCentBin(centr);
+      if (beam.first.pid() == 1000290630 && beam.second.pid() == 1000290630) 
+      {
+          CollSystem = "CuCu";
+          nNucleons = 63.;
+      }
+      //if (beam.first.pid() == 1000290630 && beam.second.pid() == 1000010020) CollSystem = "dAu";
+      //if (beam.first.pid() == 1000010020 && beam.second.pid() == 1000290630) CollSystem = "dAu";
+      if(CollSystem.compare("Empty") == 0) return;
+      
+      string cmsEnergy = "Empty";
+      if (fuzzyEquals(sqrtS()/GeV, 200*nNucleons, 1E-3)) cmsEnergy = "200GeV";
+      if (fuzzyEquals(sqrtS()/GeV, 62*nNucleons, 1E-3)) cmsEnergy = "62GeV";
+      if(cmsEnergy.compare("Empty") == 0) return;
+      
+      string SysAndEnergy = CollSystem + cmsEnergy;
 
-      vector<int> triggerPhis[numTrigPtBins];
+      // Prepare centrality projection and value
+      const CentralityProjection& centProj = apply<CentralityProjection>(event,"CMULT");
+      double centr = centProj();
+    
+    double triggerptMin = 999.;
+    double triggerptMax = -999.;
+    double associatedptMin = 999.;
+    double associatedptMax = -999.;
+    
+    bool isVeto = true;
+    
+    for(Correlator& corr : Correlators)
+    {
+        if(!corr.CheckCollSystemAndEnergy(SysAndEnergy)) continue;
+        if(!corr.CheckCentrality(centr)) continue;
+        
+        //If event is accepted for the correlator, fill event weights
+        sow[corr.GetIndex()]->fill();
+        nEvents[corr.GetIndex()]++;
+        
+        isVeto = false;
+        
+        //Check min and max of the trigger and associated particles in order to speed up the particle loops
+        if(corr.GetTriggerRangeMin() < triggerptMin) triggerptMin = corr.GetTriggerRangeMin();
+        if(corr.GetTriggerRangeMax() > triggerptMax) triggerptMax = corr.GetTriggerRangeMax();
+        
+        if(corr.GetAssociatedRangeMin() < associatedptMin) associatedptMin = corr.GetAssociatedRangeMin();
+        if(corr.GetAssociatedRangeMax() > associatedptMax) associatedptMax = corr.GetAssociatedRangeMax();
+    }
+    
+    if(isVeto) vetoEvent;
+    
     // loop over charged final state particles
       for(const Particle& pTrig : cfsTrig.particles()) {
 
-	       // protections against mc generators decaying long-lived particles
-	        if (!( pTrig.hasAncestor(310) || pTrig.hasAncestor(-310)  ||     // K0s
-	           pTrig.hasAncestor(130)  || pTrig.hasAncestor(-130)  ||     // K0l
-	           pTrig.hasAncestor(3322) || pTrig.hasAncestor(-3322) ||     // Xi0
-	           pTrig.hasAncestor(3122) || pTrig.hasAncestor(-3122) ||     // Lambda
-	           pTrig.hasAncestor(3222) || pTrig.hasAncestor(-3222) ||     // Sigma+/-
-	           pTrig.hasAncestor(3312) || pTrig.hasAncestor(-3312) ||     // Xi-/+
-	           pTrig.hasAncestor(3334) || pTrig.hasAncestor(-3334) ))    // Omega-/+
-	        {
+	       if(pTrig.pt()/GeV < triggerptMin || pTrig.pt()/GeV > triggerptMax) continue;
+
+          //Check if is secondary
+          if(isSecondary(pTrig)) continue;
+          
 		  if( abs(pTrig.pid())==211 || abs(pTrig.pid())==2212 || abs(pTrig.pid())==321){
 
-
+            for(Correlator& corr : Correlators)
+            {
+                if(!corr.CheckConditions(SysAndEnergy, centr, pTrig.pt()/GeV)) continue;
+                nTriggers[corr.GetIndex()]++;
+            }
 
 		    for(const Particle& pAssoc : cfs.particles()) {
-		      
-		      // protections against mc generators decaying long-lived particles
-		      if (!( pAssoc.hasAncestor(310) || pAssoc.hasAncestor(-310)  ||     // K0s
-			     pAssoc.hasAncestor(130)  || pAssoc.hasAncestor(-130)  ||     // K0l
-			     pAssoc.hasAncestor(3322) || pAssoc.hasAncestor(-3322) ||     // Xi0
-			     pAssoc.hasAncestor(3122) || pAssoc.hasAncestor(-3122) ||     // Lambda
-			     pAssoc.hasAncestor(3222) || pAssoc.hasAncestor(-3222) ||     // Sigma+/-
-			     pAssoc.hasAncestor(3312) || pAssoc.hasAncestor(-3312) ||     // Xi-/+
-			     pAssoc.hasAncestor(3334) || pAssoc.hasAncestor(-3334) ))    // Omega-/+
-			{
+                
+                if(pAssoc.pt()/GeV < associatedptMin || pAssoc.pt()/GeV > pTrig.pt()/GeV) continue;
+                
+                //Check if Trigger and Associated are the same particle
+                if(isSameParticle(pTrig,pAssoc)) continue;
+                
+                //Check if is secondary
+                if(isSecondary(pAssoc)) continue;
+                
 			  if( abs(pAssoc.pid())==211 || abs(pAssoc.pid())==2212 || abs(pAssoc.pid())==321){
-			    int mybin = GetTrigBin(pTrig.pt());
-			    int mybina = GetAssocBin(pAssoc.pt());
+			    //int mybin = GetTrigBin(pTrig.pt());
+			    //int mybina = GetAssocBin(pAssoc.pt());
 
 			    //https://rivet.hepforge.org/code/dev/structRivet_1_1DeltaPhiInRange.html
 			    double dPhi = deltaPhi(pTrig, pAssoc, true);//this does NOT rotate the delta phi to be in a given range
 			    double dEta = deltaEta(pTrig, pAssoc);
-			    cout<<" pTTrig "<<pTrig.momentum().pt()<<" pTassoc "<<pAssoc.momentum().pt()<<"dphi "<<dPhi<<" dEta "<<dEta<<" trigpT bin "<<mybin<<" bina "<<mybina<<endl;
-			    //for this analysis we divide by the acceptance in delta eta as well when we fill
-			    double acceptance  = -dEta/2.0 + 1.0;
-			    //fill histogram with 1.0/acceptance
-			  }
-			}
-		    }
-
-
-		    //centr is centrality in percentile
-		    //triggerPhis.push_back(p.momentum().pt()); 
-
-
-		  }	          // particle switch
-         } // primary pi, K, p only
-
+			    
+                for(Correlator& corr : Correlators)
+                {
+                    if(!corr.CheckConditionsMaxTrigger(SysAndEnergy, centr, pTrig.pt()/GeV, pAssoc.pt()/GeV)) continue;
+                    
+                    if(abs(dPhi) < 0.78)
+                    {
+                        _h["031" + to_string(corr.GetIndex())]->fill(-abs(dEta), 0.5);
+                    }
+                    
+                    if(abs(dEta) < 0.78)
+                    {
+                        _h["041" + to_string(corr.GetIndex())]->fill(-abs(dPhi), 0.5);
+                    }
+                    
+                } //end of correlators loop 
+                
+			  } // associated hadrons
+		    } // end of loop over associated particles
+		  } // trigger hadrons
       } // particle loop
 
     }
@@ -288,6 +445,17 @@ namespace Rivet {
 
     /// Normalise histograms etc., after the run
     void finalize() {
+        
+        for(unsigned int i = 1; i <= Correlators.size(); i++)
+        {
+            if(nTriggers[i] > 0)
+            {
+                _h["031" + to_string(i)]->scaleW((double)nEvents[i]/(nTriggers[i]*sow[i]->sumW()));
+                _h["041" + to_string(i)]->scaleW((double)nEvents[i]/(nTriggers[i]*sow[i]->sumW()));
+            }
+            
+        }
+        
       //normalize correlation histograms by scaling by 1.0/(Ntrig*binwidthphi*binwidtheta) in each bin BUT also be careful when rebinning.  Probably best to FIRST add histograms for correlation functions THEN normalize
       //do background subtraction ala zyam
       //calculate yields
@@ -297,16 +465,13 @@ namespace Rivet {
 
     }
 
-    //@}
-
-
-    /// name Histograms
-    //{
+    //Histograms and variables
     map<string, Histo1DPtr> _h;
-    //map<string, Profile1DPtr> _p;
-    //map<string, CounterPtr> _c;
-    //}
-
+    map<int, CounterPtr> sow;
+    bool fillTrigger = true;
+    vector<int> nTriggers;
+    vector<int> nEvents;
+    vector<Correlator> Correlators;
 
   };
 
