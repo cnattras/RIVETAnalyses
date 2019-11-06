1109a4cd069012898775608a46902bb5c2fc9ce1
eirwin3 <54359317+eirwin3@users.noreply.github.com>
2019-11-06 13:14:26 -0500

    Erica's Code Updates

--- 1604.01117/STAR_2016_I1442357.cc
+++ 1604.01117/STAR_2016_I1442357.cc
@@ -10,7 +10,10 @@
 #include <iostream>
 #include <math.h>
 #include <vector> 
+#include "RHICCentrality.hh"
 #define _USE_MATH_DEFINES
+
+/*
 // Delta_phi(rad) bins
 static const int numDelta_phiBins = 29;
 static const float Delta_phiBins[] = {-1.4,-1.2,-1.0,-0.8,-0.6,-0.4,-0.2,0.0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.2,4.4,4.6};
@@ -30,8 +33,92 @@ static const float pTAssocBins[] = {0.0,3.0,6.0,9.0};
 static const int numCentBins = 9;
 static const int centBins[] = {0,5,10,20,30,40,50,60,70,80};
 
+*/
+
 using namespace std;
+
 namespace Rivet {
+   
+   //------------------------------------------------------------------------------------------------------------------------ 
+    class Correlator {
+  
+  public:
+    
+    /// Constructor
+
+    Correlator(int index) {
+      _index = index;
+    }
+
+    void SetCollSystemAndEnergy(string s){ _collSystemAndEnergy = s; }
+    void SetCentrality(double cmin, double cmax){ _centrality = make_pair(cmin, cmax); }
+    void SetTriggerRange(double tmin, double tmax){ _triggerRange = make_pair(tmin, tmax); }
+    void SetAssociatedRange(double amin, double amax){ _associatedRange = make_pair(amin, amax); }
+    
+    string GetCollSystemAndEnergy(){ return _collSystemAndEnergy; }
+    pair<double,double> GetCentrality(){ return _centrality; }
+    double GetCentralityMin(){ return _centrality.first; }
+    double GetCentralityMax(){ return _centrality.second; }
+    pair<double,double> GetTriggerRange(){ return _triggerRange; }
+    double GetTriggerRangeMin(){ return _triggerRange.first; }
+    double GetTriggerRangeMax(){ return _triggerRange.second; }
+    pair<double,double> GetAssociatedRange(){ return _associatedRange; }
+    double GetAssociatedRangeMin(){ return _associatedRange.first; }
+    double GetAssociatedRangeMax(){ return _associatedRange.second; }
+    
+    int GetIndex(){ return _index; }
+    
+    bool CheckCollSystemAndEnergy(string s){ return _collSystemAndEnergy.compare(s) == 0 ? true : false; }
+    bool CheckCentrality(double cent){ return (cent>_centrality.first && cent<_centrality.second) ? true : false; }
+    bool CheckTriggerRange(double tpt){ return (tpt>_triggerRange.first && tpt<_triggerRange.second) ? true : false; }
+    bool CheckAssociatedRange(double apt){ return (apt>_associatedRange.first && apt<_associatedRange.second) ? true : false; }
+    bool CheckAssociatedRangeMaxTrigger(double apt, double tpt){ return (apt>_associatedRange.first && apt<tpt) ? true : false; }
+    bool CheckConditions(string s, double cent, double tpt, double apt)
+
+    {
+        if(!CheckConditions(s, cent, tpt)) return false;
+        if(!CheckAssociatedRange(apt)) return false;
+        
+        return true;
+    }
+
+    bool CheckConditions(string s, double cent, double tpt)
+    {
+        if(!CheckConditions(s, cent)) return false;
+        if(!CheckTriggerRange(tpt)) return false;
+        
+        return true;
+    }
+
+    bool CheckConditions(string s, double cent)
+    {
+        if(!CheckCollSystemAndEnergy(s)) return false;
+        if(!CheckCentrality(cent)) return false;
+        
+        return true;
+    }
+
+    bool CheckConditionsMaxTrigger(string s, double cent, double tpt, double apt)
+    {
+        if(!CheckCollSystemAndEnergy(s)) return false;
+        if(!CheckCentrality(cent)) return false;
+        if(!CheckTriggerRange(tpt)) return false;
+        if(!CheckAssociatedRangeMaxTrigger(apt,tpt)) return false;
+
+        return true;
+    }
+
+    int _index;
+    string _collSystemAndEnergy;
+    pair<double,double> _centrality;
+    pair<double,double> _triggerRange;
+    pair<double,double> _associatedRange;
+
+  };
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
 
   class STAR_2016_I1442357 : public Analysis {
   public:
@@ -39,7 +126,7 @@ namespace Rivet {
     /// Constructor
     DEFAULT_RIVET_ANALYSIS_CTOR(STAR_2016_I1442357);
 
-
+/*
     /// name Analysis methods
     // zT
     int GetzTBin(float zT){
@@ -86,14 +173,22 @@ namespace Rivet {
       cerr<<"Warning: cent "<<cent<<" is greater than the maximum centrality bin!"<<endl;
       return -1;
     }
+    */
+
+
+
+
+
+
+
     /// Book histograms and initialise projections before the run
-    void init() {              /// ******** ADD PROJECTION FOR PHOTONS ***********************
+    void init() {             
 
       // Initialise and register projections
 
       // the basic final-state projection: all final-state particles within the given eta acceptance
 
-      
+
       const ChargedFinalState cfs(Cuts::abseta < 1.0 && Cuts::pT > 1*GeV);
       declare(cfs, "CFS");
       const ChargedFinalState cfsTrig(Cuts::abseta < 1.0 && Cuts::pT > 2*GeV);
@@ -116,7 +211,7 @@ namespace Rivet {
    book(_h["0111"], 1, 1, 1); //AuAu, 1.2<pT^assoc<3 GeV/c, gamma
    book(_h["0211"], 2, 1, 1); //AuAu, 1.2<pT^assoc<3 GeV/c, pi0
    //Data from Fig. 1b
-	 book(_h["0311"], 3, 1, 1); //AuAu, 3<pT^assoc< 5 GeV/c, gamma
+	 book(_h["0311"], 3, 1, 1); //AuAu, 3<pT^assoc<5 GeV/c, gamma
    book(_h["0411"], 4, 1, 1); //AuAu, 3<pT^assoc<5 GeV/c, pi0
    //Data from Fig. 1c
    book(_h["0511"], 5, 1, 1); //pp, 1.2<pT^assoc<3 GeV/c, gamma
@@ -127,6 +222,8 @@ namespace Rivet {
    
    //*****FIGURE 2, zT //pi0-hadron correlations**************************************************
    //D(zT)
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    //Data from Fig. 2a
    book(_h["0911"], 9, 1, 1);  //Away-side, Au+Au
    book(_h["1011"], 10, 1, 1); //Away-side, p+p
@@ -136,25 +233,35 @@ namespace Rivet {
    
    //*****FIGURE 3, zT //Direct Photon-hadron correlations****************************************
    //D(zT)
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    book(_h["1311"], 13, 1, 1); //Away-side, AuAu 
    book(_h["1411"], 14, 1, 1); //Away-side, pp
    
    //*****FIGURE 4, zT_corr **********************************************************************
    //D(zT_Corr)
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    book(_h["1511"], 15, 1, 1); //Pi0-hadron correlations, Away-side, pp
    
    //*****FIGURE 5, zT ***************************************************************************
    //I_AA
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    book(_h["1611"], 16, 1, 1); //gamma, Away-side, I_AA
    book(_h["1711"], 17, 1, 1); //pi0, Away-side, I_AA
    
    //*****FIGURE 6, zT ***************************************************************************
    //Ratio
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    book(_h["1811"], 18, 1, 1); //gamma
    book(_h["1911"], 19, 1, 1); //pi0
    
    //*****FIGURE 7 *******************************************************************************
    //I_AA
+   //12<pT^trig<20 GeV/c
+   //1.2<pT^assoc GeV/c
    book(_h["2011"], 20, 1, 1); //gamma, Away-side, I_AA (pT^trig)
    book(_h["2111"], 21, 1, 1); //gamma, Away-side, I_AA (pT^assoc)
    
