c18d326d3b89e1670ccfb0d0cd544b8c044f10e6
Will Witt <wwitt4@vols.utk.edu>
2020-05-13 16:07:27 -0400

    Adding the latest jet v2 code for Antonio to run over ANGATYR

--- JETV2/RunAnalysis.sh
+++ JETV2/RunAnalysis.sh
@@ -2,7 +2,7 @@
 rm *.so
 rivet-buildplugin RivetALICE_2019_JETV2.so ALICE_2019_JETV2.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -a ALICE_2019_JETV2 --ignore-beams /home/william/JEWEL/jewel-2.2.0/eventfiles/jewel.Pb-Pb.2760.30-50cent.*.hepmc
-#rivet --pwd -a ALICE_2019_JETV2 --ignore-beams /home/william/JEWEL/jewel-2.2.0/eventfiles/jewel.Pb-Pb.2760.0-5cent.01.hepmc
-#rivet --pwd -a ALICE_2019_JETV2 --ignore-beams /home/william/JEWEL/jewel-2.2.0/eventfiles/2357154.apollo-acf/*/*.hepmc
+#rivet --pwd -a ALICE_2019_JETV2 --ignore-beams /home/william/JEWEL/jewel-2.2.0/eventfiles/Merge_Test/*/*/merged.hepmc
+#rivet --pwd -a ALICE_2019_JETV2 --ignore-beams /media/william/Seagate\ Expansion\ Drive/Jewel_Data/5020.10-30.20M/*/merged.hepmc
+rivet --pwd -a ALICE_2019_JETV2 --ignore-beams ~/ANGATYR/*.hepmc
 rivet-mkhtml --pwd --errs Rivet.yoda
