915eaf65f839b63e6ffba7302864706ab21c0753
cnattras <saccharomyces.cerevisae@gmail.com>
2020-06-16 17:07:03 -0400

    adding scripts to do everything

---
+++ 1604.01117/RunAnalysis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SAMPLE_FILE=../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+#This compiles the code
+rivet-build RivetSTAR_2016_I1442357.so STAR_2016_I1442357.cc
+#This runs it
+rivet --pwd -p calibration.yoda -a STAR_2016_I1442357:cent=GEN $SAMPLE_FILE
