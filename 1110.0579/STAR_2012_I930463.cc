86728918707375966d412a19c1c073286df89cb9
cbaillar <50589854+cbaillar@users.noreply.github.com>
2020-07-23 13:33:16 -0400

    Add files via upload

---
+++ 1110.0579/STAR_2012_I930463.cc
@@ -0,0 +1,681 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/PrimaryParticles.hh"
+#include "Rivet/Projections/FastJets.hh"
+#include "Rivet/Projections/DressedLeptons.hh"
+#include "Rivet/Projections/MissingMomentum.hh"
+#include "Rivet/Projections/PromptFinalState.hh"
+#include "Rivet/Tools/Cuts.hh"
+#include "Rivet/Projections/SingleValueProjection.hh"
+#include "Rivet/Projections/CentralityProjection.hh"
+#include "Rivet/Tools/AliceCommon.hh"
+#include "Rivet/Projections/AliceCommon.hh"
+#include "Centrality/RHICCentrality.hh" //external header for Centrality calculation
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#define _USE_MATH_DEFINES
+
+namespace Rivet {
+
+
+  /// @brief Add a short analysis description here
+  class STAR_2012_I930463 : public Analysis {
+  public:
+
+    /// Constructor
+    DEFAULT_RIVET_ANALYSIS_CTOR(STAR_2012_I930463);
+
+
+	void init() {
+		std::initializer_list<int> pdgIds = { 321, -321, 211, -211, 2212, -2212, 310, 113 };  // pi+ 211  K+ 321   proton 2212	K0S 310		rho0 113
+
+
+
+		//charged particles
+		const PrimaryParticles cp(pdgIds, Cuts::absrap < 0.5 && Cuts::abscharge > 0);
+		declare(cp, "cp");
+
+		//neutral particles (changed fs to np)
+		const PrimaryParticles np(pdgIds, Cuts::absrap < 0.5 && Cuts::abscharge == 0);
+		declare(np, "np");
+
+		beamOpt = getOption<string>("beam", "NONE");
+
+		if (beamOpt == "PP") collSys = pp;
+		else if (beamOpt == "AUAU200") collSys = AuAu200;
+
+
+		if (!(collSys == pp)) declareCentrality(RHICCentrality("STAR"), "RHIC_2019_CentralityCalibration:exp=STAR", "CMULT", "CMULT");
+
+
+		book(sow["sow_pp"], "sow_pp");
+		book(sow["sow_AuAuc12"], "sow_AuAuc12");
+
+		//Figure 1 Yield pp
+		book(hPionPosPt["ptyieldspp"], 1, 1, 1);
+		book(hPionNegPt["ptyieldspp"], 1, 1, 2);
+		book(hKaonPosPt["ptyieldspp"], 1, 1, 3);
+		book(hKaonNegPt["ptyieldspp"], 1, 1, 4);
+		book(hProtPosPt["ptyieldspp"], 1, 1, 5);
+		book(hProtNegPt["ptyieldspp"], 1, 1, 6);
+		book(hKaon0SPt["ptyieldspp"], 2, 1, 1);
+		book(hRho0Pt["ptyieldspp"], 3, 1, 1);
+		
+		//Figure 1 Yield AUAU
+		book(hKpPosPt["ptyieldsAuAuc12"], 4, 1, 1);
+		book(hKpNegPt["ptyieldsAuAuc12"], 4, 1, 2);
+		book(hRho0Pt["ptyieldsAuAuc12"], 5, 1, 1);
+		book(hKaon0SPt["ptyieldsAuAuc12"], 6, 1, 1);
+
+		//Figure 2 Yield Ratio pp
+		string refname1 = mkAxisCode(7, 1, 1);
+		const Scatter2D& refdata1 = refData(refname1);
+		book(hPionNegPt["pp1"], refname1 + "_PionNeg", refdata1);
+		book(hPionPosPt["pp1"], refname1 + "_PionPos", refdata1);
+		book(RatioPion["pp"], refname1);
+
+		string refname2 = mkAxisCode(7, 1, 2);
+		const Scatter2D& refdata2 = refData(refname2);
+		book(hProtNegPt["pp1"], refname2 + "_ProtNeg", refdata2);
+		book(hProtPosPt["pp1"], refname2 + "_ProtPos", refdata2);
+		book(RatioProt["pp"], refname2);
+
+		string refname3 = mkAxisCode(7, 1, 3);
+		const Scatter2D& refdata3 = refData(refname3);
+		book(hKaonNegPt["pp"], refname3 + "_KaonNeg", refdata3);
+		book(hKaonPosPt["pp"], refname3 + "_KaonPos", refdata3);
+		book(RatioKaon["pp"], refname3);
+
+		string refname4 = mkAxisCode(7, 1, 4);
+		const Scatter2D& refdata4 = refData(refname4);
+		book(hProtPosPt["pp2"], refname4 + "_ProtPos", refdata4);
+		book(hPionPosPt["pp2"], refname4 + "_PionPos", refdata4);
+		book(Ratioppipos["pp"], refname4);
+
+		string refname5 = mkAxisCode(7, 1, 5);
+		const Scatter2D& refdata5 = refData(refname5);
+		book(hProtNegPt["pp2"], refname5 + "_ProtNeg", refdata5);
+		book(hPionNegPt["pp2"], refname5 + "_PionNeg", refdata5);
+		book(Ratioppineg["pp"], refname5);
+
+		string refname6 = mkAxisCode(7, 1, 6);
+		const Scatter2D& refdata6 = refData(refname6);
+		book(hKaonPt["pp"], refname6 + "_Kaon", refdata6);
+		book(hPionPt["pp1"], refname6 + "_Pion", refdata6);
+		book(RatioKpi["pp"], refname6);
+
+		string refname7 = mkAxisCode(8, 1, 1);
+		const Scatter2D& refdata7 = refData(refname7);
+		book(hKaon0SPt["pp"], refname7 + "_Kaon0S", refdata7);
+		book(hPionPt["pp2"], refname7 + "_Pion", refdata7);
+		book(RatioK0spi["pp"], refname7);
+
+		//Figure 2 Yield Ratio AUAU
+		string refname8 = mkAxisCode(9, 1, 1);
+		const Scatter2D& refdata8 = refData(refname8);
+		book(hProtPosPt["AuAuc12"], refname8 + "_ProtPos", refdata8);
+		book(hPionPosPt["AuAuc12"], refname8 + "_PionPos", refdata8);
+		book(Ratioppipos["AuAuc12"], refname8);
+
+		string refname9 = mkAxisCode(9, 1, 2);
+		const Scatter2D& refdata9 = refData(refname9);
+		book(hProtNegPt["AuAuc12"], refname9 + "_ProtNeg", refdata9);
+		book(hPionNegPt["AuAuc12"], refname9 + "_PionNeg", refdata9);
+		book(Ratioppineg["AuAuc12"], refname9);
+
+		//Figure 3 RAA
+
+		string refname10 = mkAxisCode(10, 1, 1);
+		const Scatter2D& refdata10 = refData(refname10);
+		book(hPionPt["Raa_c12_AuAu"], refname10 + "_AuAu", refdata10);
+		book(hPionPt["Raa_c12_pp"], refname10 + "_pp", refdata10);
+		book(hRaa["pi_c12_AuAu"], refname10);
+
+		string refname11 = mkAxisCode(11, 1, 1);
+		const Scatter2D& refdata11 = refData(refname11);
+		book(hKpPt["Raa_c12_AuAu"], refname11 + "_AuAu", refdata11);
+		book(hKpPt["Raa_c12_pp"], refname11 + "_pp", refdata11);
+		book(hRaa["Kp_c12_AuAu"], refname11);
+
+		string refname12 = mkAxisCode(12, 1, 1);
+		const Scatter2D& refdata12 = refData(refname12);
+		book(hKaon0SPt["Raa_c12_AuAu"], refname12 + "_AuAu", refdata12);
+		book(hKaon0SPt["Raa_c12_pp"], refname12 + "_pp", refdata12);
+		book(hRaa["K0S_c12_AuAu"], refname12);
+
+		string refname13 = mkAxisCode(13, 1, 1);
+		const Scatter2D& refdata13 = refData(refname13);
+		book(hRho0Pt["Raa_c12_AuAu"], refname13 + "_AuAu", refdata13);
+		book(hRho0Pt["Raa_c12_pp"], refname13 + "_pp", refdata13);
+		book(hRaa["Rho0_c12_AuAu"], refname13);
+
+		//Figure 3 RAA Ratio ?? Best way to do Raa ratio?
+
+		
+    }
+
+
+   
+    void analyze(const Event& event) {
+		Particles chargedParticles = applyProjection<PrimaryParticles>(event, "cp").particles();
+		Particles neutralParticles = applyProjection<PrimaryParticles>(event, "np").particles();
+		
+		if (collSys == pp)
+		{
+			sow["sow_pp"]->fill();
+			for (Particle p : chargedParticles)
+			{
+				double partPt = p.pT() / GeV;
+				double pt_weight = 1. / (partPt * 2. * M_PI);
+
+				switch (p.pid()) {						
+
+				case 211: // pi+
+				{
+					hPionPosPt["ptyieldspp"]->fill(partPt, pt_weight);					
+					hPionPosPt["pp1"]->fill(partPt);
+					hPionPosPt["pp2"]->fill(partPt);
+					hPionPt["pp1"]->fill(partPt);
+					hPionPt["pp2"]->fill(partPt);
+					hPionPt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				case -211: // pi-
+				{
+					hPionNegPt["ptyieldspp"]->fill(partPt, pt_weight);
+					hPionNegPt["pp1"]->fill(partPt);
+					hPionNegPt["pp2"]->fill(partPt);
+					hPionPt["pp1"]->fill(partPt);
+					hPionPt["pp2"]->fill(partPt);
+					hPionPt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				case  321: // K+
+				{
+					hKaonPosPt["ptyieldspp"]->fill(partPt, pt_weight);					
+					hKaonPosPt["pp"]->fill(partPt);
+					hKaonPt["pp"]->fill(partPt);
+
+					break;
+				}
+				case  -321: // K-
+				{
+					hKaonNegPt["ptyieldspp"]->fill(partPt, pt_weight);
+					hKaonNegPt["pp"]->fill(partPt);
+					hKaonPt["pp"]->fill(partPt);
+					hKpPt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				case 2212: // proton
+				{
+					hProtPosPt["ptyieldspp"]->fill(partPt, pt_weight);					
+					hProtPosPt["pp1"]->fill(partPt);
+					hProtPosPt["pp2"]->fill(partPt);
+					hKpPt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				case -2212: // anti-proton
+				{
+					hProtNegPt["ptyieldspp"]->fill(partPt, pt_weight);
+					hProtNegPt["pp1"]->fill(partPt);
+					hProtNegPt["pp2"]->fill(partPt);
+					break;
+				}
+				}
+			}
+
+			for (Particle p : neutralParticles)
+			{
+				double partPt = p.pT() / GeV;
+				double pt_weight = 1. / (partPt * 2. * M_PI);
+
+				switch (p.pid()) {
+				case 310: // K0S
+				{
+					hKaon0SPt["ptyieldspp"]->fill(partPt, pt_weight);
+					hKaon0SPt["pp"]->fill(partPt);
+					hKaon0SPt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				case 113: // rho0
+				{
+					hRho0Pt["ptyieldspp"]->fill(partPt, pt_weight);
+					hRho0Pt["Raa_c12_pp"]->fill(partPt);
+					break;
+				}
+				}
+			}
+
+			return;
+		}
+
+
+		const CentralityProjection& cent = apply<CentralityProjection>(event, "CMULT");
+		const double c = cent();
+
+		if (collSys == AuAu200)
+						
+		{
+			if ((c < 0.) || (c > 12.)) vetoEvent;
+			sow["sow_AuAuc12"]->fill();
+			for (Particle p : chargedParticles)
+			{				
+				double partPt = p.pT() / GeV;
+				double pt_weight = 1. / (partPt * 2. * M_PI);
+
+				switch (p.pid()) {
+				case 211: // pi+
+				{
+					hPionPosPt["AuAuc12"]->fill(partPt);
+					hPionPt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				case -211: // pi-
+				{
+					hPionNegPt["AuAuc12"]->fill(partPt);
+					hPionPt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				case  321: // K+
+				{
+					hKpPosPt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hKpPt["Raa_c12_AuAu"]->fill(partPt);
+
+					break;
+				}
+				case  -321: // K-
+				{
+					hKpNegPt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hKpPt["Raa_c12_AuAu"]->fill(partPt);
+
+					break;
+				}
+				case 2212: // proton
+				{
+					hKpPosPt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hProtPosPt["AuAuc12"]->fill(partPt);
+					hKpPt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				case -2212: // anti-proton
+				{
+					hKpNegPt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hProtNegPt["AuAuc12"]->fill(partPt);
+					hKpPt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				}
+			}
+
+			for (Particle p : neutralParticles)
+			{
+				double partPt = p.pT() / GeV;
+				double pt_weight = 1. / (partPt * 2. * M_PI);
+
+				switch (p.pid()) {
+				case 310: // K0S
+				{
+					hKaon0SPt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hKaon0SPt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				case 113: // rho0
+				{
+					hRho0Pt["ptyieldsAuAuc12"]->fill(partPt, pt_weight);
+					hRho0Pt["Raa_c12_AuAu"]->fill(partPt);
+					break;
+				}
+				}
+			}
+
+			return;
+		}
+     
+
+    }
+
+
+    void finalize() {
+		bool AuAu200_available = false;
+		bool pp_available = false;
+
+		for (auto element : hKaonNegPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hKaonPosPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hPionNegPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hPionPosPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hProtNegPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hProtPosPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hKaonPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hPionPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hProtPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}			
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hKaon0SPt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+		for (auto element : hRho0Pt)
+		{
+			string name = element.second->name();
+			if (name.find("AuAu") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) AuAu200_available = true;
+				else
+				{
+					AuAu200_available = false;
+					break;
+				}
+			}
+			else if (name.find("pp") != std::string::npos)
+			{
+				if (element.second->numEntries() > 0) pp_available = true;
+				else
+				{
+					pp_available = false;
+					break;
+				}
+			}
+		}
+
+		if (!(AuAu200_available && pp_available)) return;
+
+
+		//Figure 1 Yield pp
+		hPionPosPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hPionNegPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hKaonPosPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hKaonNegPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hProtPosPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hProtNegPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hKaon0SPt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		hRho0Pt["ptyieldspp"]->scaleW(1. / sow["sow_pp"]->sumW());
+
+		//Figure 1 Yield AUAU
+		hKpPosPt["ptyieldsAuAuc12"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hKpNegPt["ptyieldsAuAuc12"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hRho0Pt["ptyieldsAuAuc12"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hKaon0SPt["ptyieldsAuAuc12"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+
+		//Figure 2 Yield Ratio pp
+		divide(hPionNegPt["pp1"], hPionPosPt["pp1"], RatioPion["pp"]);				
+		divide(hProtNegPt["pp1"], hProtPosPt["pp1"], RatioProt["pp"]);		
+		divide(hKaonNegPt["pp"], hKaonPosPt["pp"], RatioKaon["pp"]);		
+		divide(hProtPosPt["pp2"], hPionPosPt["pp2"], Ratioppipos["pp"]);		
+		divide(hProtNegPt["pp2"], hPionNegPt["pp2"], Ratioppineg["pp"]);		
+		divide(hKaonPt["pp"], hPionPt["pp1"], RatioKpi["pp"]);		
+		divide(hKaon0SPt["pp"], hPionPt["pp2"], RatioK0spi["pp"]);
+
+		//Figure 2 Yield Ratio AUAU
+		divide(hProtPosPt["AuAuc12"], hPionPosPt["AuAuc12"], Ratioppipos["AuAuc12"]);		
+		divide(hProtNegPt["AuAuc12"], hPionNegPt["AuAuc12"], Ratioppineg["AuAuc12"]);
+
+		//Figure 3 RAA
+		hPionPt["Raa_c12_AuAu"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hPionPt["Raa_c12_pp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		divide(hPionPt["Raa_c12_AuAu"], hPionPt["Raa_c12_pp"], hRaa["pi_c12_AuAu"]));
+		//hRaa["pi_c12_AuAu"]->scaleY(1. / 960.2);
+		
+		hKpPt["Raa_c12_AuAu"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hKpPt["Raa_c12_pp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		divide(hKpPt["Raa_c12_AuAu"], hKpPt["Raa_c12_pp"], hRaa["Kp_c12_AuAu"]);
+		//hRaa["Kp_c12_AuAu"]->scaleY(1. / 960.2);
+		
+		hKaon0SPt["Raa_c12_AuAu"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hKaon0SPt["Raa_c12_pp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		divide(hKaon0SPt["Raa_c12_AuAu"], hKaon0SPt["Raa_c12_pp"], hRaa["K0S_c12_AuAu"]);
+		//hRaa["K0S_c12_AuAu"]->scaleY(1. / 960.2);
+
+		hRho0Pt["Raa_c12_AuAu"]->scaleW(1. / sow["sow_AuAuc12"]->sumW());
+		hRho0Pt["Raa_c12_pp"]->scaleW(1. / sow["sow_pp"]->sumW());
+		divide(hRho0Pt["Raa_c12_AuAu"], hRho0Pt["Raa_c13_pp"], hRaa["Rho0_c13_AuAu"]);
+		//hRaa["Rho0_c13_AuAu"]->scaleY(1. / 960.2);
+
+
+    }
+
+
+	map<string, Histo1DPtr> hKaonNegPt;
+	map<string, Histo1DPtr> hKaonPosPt;
+	map<string, Histo1DPtr> hPionNegPt;
+	map<string, Histo1DPtr> hPionPosPt;
+	map<string, Histo1DPtr> hProtNegPt;
+	map<string, Histo1DPtr> hProtPosPt;
+	map<string, Histo1DPtr> hKaon0SPt;
+	map<string, Histo1DPtr> hRho0Pt;
+	map<string, Histo1DPtr> hKpNegPt;
+	map<string, Histo1DPtr> hKpPosPt;
+	map<string, Histo1DPtr> hKpPt;
+
+	map<string, Histo1DPtr> hKaonPt;
+	map<string, Histo1DPtr> hPionPt;
+	map<string, Histo1DPtr> hProtPt;
+
+
+	
+	map<string, Scatter2DPtr> RatioPion;
+	map<string, Scatter2DPtr> RatioProt;
+	map<string, Scatter2DPtr> RatioKaon;
+	map<string, Scatter2DPtr> Ratioppipos;
+	map<string, Scatter2DPtr> Ratioppineg;
+
+	map<string, Scatter2DPtr> hRaa;
+		
+
+	map<string, CounterPtr> sow;
+	string beamOpt;
+	enum CollisionSystem { pp, AuAu200 };
+	CollisionSystem collSys;
+
+  };
+
+
+  DECLARE_RIVET_PLUGIN(STAR_2012_I930463);
+
+}
---
+++ 1110.0579/STAR_2012_I930463.plot
@@ -0,0 +1,167 @@
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y01
+Title= 'Invariant yields $\pi^+$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y02
+Title= 'Invariant yields $\pi^-$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y03
+Title= 'Invariant yields $K^+$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y04
+Title= 'Invariant yields $K^-$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y05
+Title= 'Invariant yields $p$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d01-x01-y06
+Title= 'Invariant yields $\bar{p}$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d02-x01-y01
+Title= '$K^0_S$ yields in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d03-x01-y01
+Title= '$\rho^0$ yields in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d04-x01-y01
+Title= 'Invariant yield $K^+ + p$ in Au+Au ($0-12\%$) collisions at 200 GeV.'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d04-x01-y02
+Title= 'Invariant yield $K^- + \bar{p}$ in Au+Au ($0-12\%$) collisions at 200 GeV.'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d05-x01-y01
+Title= '$\rho^0$ in Au+Au ($0-12\%$) collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d06-x01-y01
+Title= '$K^0_S$ in Au+Au ($0-12\%$) collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y01
+Title= 'Yield Ratio $\pi^-/\pi^+$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y02
+Title= 'Yield Ratio $\bar{p}/p$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y03
+Title= 'Yield Ratio $K^-/K^+$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y04
+Title= 'Yield Ratio $p/\pi^+$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y05
+Title= 'Yield Ratio $\bar{p}/\pi^-$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d07-x01-y06
+Title= 'Yield Ratio $K^{\pm}/\pi{\pm}$ in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d08-x01-y01
+Title= '$K^0_S/\pi^{\pm}$ Ratio in p+p collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d09-x01-y01
+Title= 'Yield Ratio $p/\pi^+$ in $0-12\%$ Au+Au collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio 
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d09-x01-y02
+Title= 'Yield Ratio $\bar{p}/\pi^-$ in $0-12\%$ Au+Au collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Yield Ratio
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d10-x01-y01
+Title= '$\pi^{\pm}$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=$R_{AA}$
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d11-x01-y01
+Title= '$K^{\pm}+p(\bar{p})$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=$R_{AA}$  
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d12-x01-y01
+Title= '$K^0_S$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=$R_{AA}$  
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d13-x01-y01
+Title= '$\rho^0$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=$R_{AA}$
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d14-x01-y01
+Title= '$R_{AA} K^{\pm} + p(\bar{p}) /R_{AA} \pi^{\pm}$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Ratio
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d15-x01-y01
+Title= '$R_{AA} K^- + (\bar{p}) /R_{AA} K^+ +p$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Ratio
+END PLOT
+
+BEGIN PLOT /ALICE_2013_I1222333/d16-x01-y01
+Title= '$R_{AA} \rho^0 /R_{AA} \pi^{\pm}$ in Au+Au $0-12\%$ collisions at 200 GeV'
+XLabel=$p_T$
+YLabel=Ratio
+END PLOT
