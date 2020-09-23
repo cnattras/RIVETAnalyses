94f47ae44e714328a0e1840d3c2a5779a8c48ac3
cnattras <saccharomyces.cerevisae@gmail.com>
2020-09-23 13:26:15 -0400

    Adding options to the analysis and making it a heavy ion template

--- 0604018/STAR_2006_I715470.cc
+++ 0604018/STAR_2006_I715470.cc
@@ -1,10 +1,17 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
-#include "Rivet/Projections/FastJets.hh"
-#include "Rivet/Projections/DressedLeptons.hh"
-#include "Rivet/Projections/MissingMomentum.hh"
 #include "Rivet/Projections/PromptFinalState.hh"
+#include "Rivet/Projections/ChargedFinalState.hh"
+#include "Rivet/Tools/AliceCommon.hh"
+#include "Rivet/Projections/AliceCommon.hh"
+#include "Rivet/Projections/PromptFinalState.hh"
+#include "Rivet/Projections/PrimaryParticles.hh"
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include "../Centralities/RHICCentrality.hh"
 
 namespace Rivet {
 
@@ -12,103 +19,132 @@ namespace Rivet {
   /// @brief Add a short analysis description here
   class STAR_2006_I715470 : public Analysis {
   public:
-
+ bool isSameParticle(const Particle& p1, const Particle& p2)
+      {
+        //if pT, eta and phi are equal, they are the same particle
+        if(p1.pt() != p2.pt()) return false;
+        if(p1.eta() != p2.eta()) return false;
+        if(p1.phi() != p2.phi()) return false;
+   
+        return true;
+      }
+   
+      bool isSecondary(Particle p)
+      {
+        //return true if is secondary
+        if (( p.hasAncestor(310) || p.hasAncestor(-310)  ||     // K0s
+          p.hasAncestor(130)  || p.hasAncestor(-130)  ||     // K0l
+          p.hasAncestor(3322) || p.hasAncestor(-3322) ||     // Xi0
+          p.hasAncestor(3122) || p.hasAncestor(-3122) ||     // Lambda
+          p.hasAncestor(3222) || p.hasAncestor(-3222) ||     // Sigma+/-
+          p.hasAncestor(3312) || p.hasAncestor(-3312) ||     // Xi-/+
+          p.hasAncestor(3334) || p.hasAncestor(-3334) ))    // Omega-/+
+          return true;
+        else return false;
+       
+      }
+    double CalculateVn(YODA::Histo1D& hist, int nth)
+    {
+        int nBins = hist.numBins();
+        double integral = 0.;
+       
+        double Vn = 0.;
+        for (int i = 0; i < nBins; i++)
+        {
+            integral += hist.bin(i).sumW();
+            Vn += hist.bin(i).sumW()*cos(nth*hist.bin(i).xMid());
+        }
+        Vn /= integral;
+        return Vn;
+    }
+   
+    int FindBinAtMinimum(YODA::Histo1D& hist, double bmin, double bmax)
+    {
+        int minBin = -1;
+        double minVal = 999.;
+       
+        for(unsigned int i = 0; i < hist.numBins(); i++)
+        {
+            if(hist.bin(i).xMid() < bmin || hist.bin(i).xMid() > bmax) continue;
+            if( (hist.bin(i).sumW()/hist.bin(i).xWidth()) < minVal )
+            {
+                minVal = hist.bin(i).sumW()/hist.bin(i).xWidth();
+                minBin = i;
+            }
+        }
+       
+        return minBin;
+       
+    }
+   
+    void SubtractBackground(YODA::Histo1D& fullHist, YODA::Histo1D& hist, vector<int> n, double bmin, double bmax)
+    {
+        vector<double> Vn(n.size(), 0);
+        for(unsigned int i = 0; i < n.size(); i++)
+        {
+            Vn[i] = CalculateVn(fullHist, n[i]);
+        }
+       
+        double bmod = 1.;
+        int minBin = FindBinAtMinimum(fullHist, bmin, bmax);
+       
+        for(unsigned int i = 0; i < Vn.size(); i++)
+        {
+            bmod += 2*Vn[i]*cos(n[i]*fullHist.bin(minBin).xMid());
+        }
+       
+        double b = (fullHist.bin(minBin).sumW()/fullHist.bin(minBin).xWidth())/bmod; //Divided by bin width in order to generalize it and enable it to be used for histograms with different binning
+               
+        for(unsigned int ibin = 0; ibin < hist.numBins(); ibin++)
+        {
+            double modulation = 1;
+            for(unsigned int i = 0; i < Vn.size(); i++)
+            {
+                modulation += 2*Vn[i]*cos(n[i]*hist.bin(ibin).xMid());
+            }
+            modulation *= b;
+            hist.bin(ibin).scaleW(1 - (modulation/(hist.bin(ibin).sumW()/hist.bin(ibin).xWidth()))); //Divided by bin width to compensate the calculation of "b"
+        }
+       
+    }
+   
     /// Constructor
     DEFAULT_RIVET_ANALYSIS_CTOR(STAR_2006_I715470);
-
-
-    /// @name Analysis methods
-    //@{
-
-    /// Book histograms and initialise projections before the run
     void init() {
-
-      // Initialise and register projections
-
-      // The basic final-state projection:
-      // all final-state particles within
-      // the given eta acceptance
-      const FinalState fs(Cuts::abseta < 4.9);
-
-      // The final-state particles declared above are clustered using FastJet with
-      // the anti-kT algorithm and a jet-radius parameter 0.4
-      // muons and neutrinos are excluded from the clustering
-      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
-      declare(jetfs, "jets");
-
-      // FinalState of prompt photons and bare muons and electrons in the event
-      PromptFinalState photons(Cuts::abspid == PID::PHOTON);
-      PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
-
-      // Dress the prompt bare leptons with prompt photons within dR < 0.1,
-      // and apply some fiducial cuts on the dressed leptons
-      Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
-      DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
-      declare(dressed_leps, "leptons");
-
-      // Missing momentum
-      declare(MissingMomentum(fs), "MET");
-
-      // Book histograms
-      // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
-      // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
-
+        const ChargedFinalState cfs(Cuts::abseta < 0.35);
+        declare(cfs, "CFS");
+       
+        const PrimaryParticles pp(pdgPi0, Cuts::abseta < 0.35);
+        declare(pp, "PP");
+       
+        const PromptFinalState pfs(Cuts::abseta < 0.35 && Cuts::pid == 22);
+        declare(pfs, "PFS");
     }
-
-
-    /// Perform the per-event analysis
     void analyze(const Event& event) {
-
-      // Retrieve dressed leptons, sorted by pT
-      vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
-
-      // Retrieve clustered jets, sorted by pT, with a minimum pT cut
-      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
-
-      // Remove all jets within dR < 0.2 of a dressed lepton
-      idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
-
-      // Select jets ghost-associated to B-hadrons with a certain fiducial selection
-      Jets bjets = filter_select(jets, [](const Jet& jet) {
-        return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
-      });
-
-      // Veto event if there are no b-jets
-      if (bjets.empty())  vetoEvent;
-
-      // Apply a missing-momentum cut
-      if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
-
-      // Fill histogram with leading b-jet pT
-      _h["XXXX"]->fill(bjets[0].pT()/GeV);
-
+      const ChargedFinalState& cfs = apply<ChargedFinalState>(event, "CFS");
+      const PrimaryParticles& ppTrigPi0 = apply<PrimaryParticles>(event, "PP");
+      const PromptFinalState& pfsTrigPhotons = apply<PromptFinalState>(event, "PFS");
+      //==================================================
+      // Select the histograms accordingly to the collision system, beam energy and centrality
+      // WARNING: Still not implemented for d-Au
+      //==================================================
+      double nNucleons = 0.;
+      string CollSystem = "Empty";
+      const ParticlePair& beam = beams();
+          CollSystem = "pp";
+          nNucleons = 1.;
+      //if (beam.first.pid() == 1000290630 && beam.second.pid() == 1000010020) CollSystem = "dAu";
+      //if (beam.first.pid() == 1000010020 && beam.second.pid() == 1000290630) CollSystem = "dAu";
+     
+      string cmsEnergy = "Empty";
+      if (fuzzyEquals(sqrtS()/GeV, 200*nNucleons, 1E-3)) cmsEnergy = "200GeV";
+     
+      string SysAndEnergy = CollSystem + cmsEnergy;
     }
-
-
-    /// Normalise histograms etc., after the run
     void finalize() {
-
-      normalize(_h["XXXX"]); // normalize to unity
-      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
-
     }
-
-    //@}
-
-
-    /// @name Histograms
-    //@{
-    map<string, Histo1DPtr> _h;
-    map<string, Profile1DPtr> _p;
-    map<string, CounterPtr> _c;
-    //@}
+    std::initializer_list<int> pdgPi0 = {111, -111};  // Pion 0
+    std::initializer_list<int> pdgPhoton = {22};  // Pion 0
 
 
   };
