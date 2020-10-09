b97df50708e394d20ba1d1c7323aea1f62f6a4be
cnattras <saccharomyces.cerevisae@gmail.com>
2020-10-09 13:26:32 -0400

    adding option to make plots

--- 08014545/RunAnalysis.sh
+++ 08014545/RunAnalysis.sh
@@ -3,3 +3,4 @@ SAMPLE_FILE=../testfiles/PYTHIAAuAuFileSMALLTEST.dat
 rivet-build RivetPHENIX_2008_I778396.so PHENIX_2008_I778396.cc
 #This runs it
 rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2008_I778396:cent=GEN:beam=AUAU -o Rivet.yoda $SAMPLE_FILE
+#rivet-mkhtml --pwd Rivet.yoda
