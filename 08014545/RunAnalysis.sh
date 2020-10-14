df5dc26602c04efd79c13af15f22b7fc65d66eba
Nora Meredith Bauer <nbauer1@acf-login7.acf.nics.utk.edu>
2020-10-13 21:06:39 -0400

    adding plots for 500 events

--- 08014545/RunAnalysis.sh
+++ 08014545/RunAnalysis.sh
@@ -1,6 +1,7 @@
 SAMPLE_FILE=../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+NEW_FILE=/lustre/haven/proj/UTK0019/Pythia8/pythia8243/examples/hepmc_AuAu_MB_1.hepmc
 #This compiles the code
 rivet-build RivetPHENIX_2008_I778396.so PHENIX_2008_I778396.cc
 #This runs it
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2008_I778396:cent=GEN:beam=AUAU -o Rivet.yoda $SAMPLE_FILE
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2008_I778396:cent=GEN:beam=AUAU -o Rivet.yoda $NEW_FILE
 #rivet-mkhtml --pwd Rivet.yoda
