6b0ff60c16765c3fbad0e82a5caa5c71d8e9f6f0
andimank <andimankolli@gmail.com>
2020-10-22 00:31:36 -0500

    Completing Analysis directory

---
+++ 0603010/RunAnalysis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rivet-build RivetPHENIX_2006_I711951.so PHENIX_2006_I711951.cc
+export RIVET_ANALYSIS_PATH=$PWD
+rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I711951:cent=GEN:beam=dAU200 -o Rivet.yoda hepmc_AuAu_200GeV_1.hepmc
