567ee8899782457fd1adfc1f6be64804ffde8777
cnattras <saccharomyces.cerevisae@gmail.com>
2020-11-06 13:46:17 -0500

    adding background subtraction not quite there on calculating yields

--- 0604018/STAR_2006_I715470.cc
+++ 0604018/STAR_2006_I715470.cc
@@ -256,6 +256,82 @@ namespace Rivet {
         }
        
     }
+        Histo1DPtr SubtractBackgroundZYAM(Histo1DPtr histo)
+    {
+        
+        YODA::Histo1D hist = *histo;
+        
+        double minValue = sqrt(-2);
+        double binWidth = 0.;
+        int minValueEntries = 0.;
+        
+        for(auto &bin : hist.bins())
+        {
+            if(std::isnan(minValue))
+            {
+                minValue = bin.sumW();
+                binWidth = bin.width();
+                minValueEntries = bin.numEntries();
+            }
+            if(bin.sumW()/bin.width() < minValue/binWidth)
+            {
+                minValue = bin.sumW();
+                binWidth = bin.width();
+                minValueEntries = bin.numEntries();
+            }
+        }
+                
+        hist.reset();
+        
+        for(auto &bin : hist.bins())
+        {
+            bin.fillBin((minValue*bin.width())/(minValueEntries*binWidth), minValueEntries);
+        }
+        
+        *histo = YODA::subtract(*histo, hist);
+        
+        return histo;
+                
+    }
+
+    
+    double getYieldRangeUser(Histo1DPtr histo, double xmin, double xmax, double &fraction)
+    {
+        //This will include bins partially covered by the user range
+        
+        YODA::Histo1D hist = *histo;
+        
+        double integral = 0.;
+        
+        if(xmax < xmin) throw RangeError("Error: xmin > xmax");
+        if(xmin < hist.bin(0).xMin()) throw RangeError("xmin is out of range");
+        if(xmax > hist.bin(hist.numBins()-1).xMax()) throw RangeError("xmax is out of range");
+        
+        for(auto &bin : hist.bins())
+        {
+            if((bin.xMin() > xmin) && (bin.xMax() < xmax))
+            {
+                integral += bin.sumW()/bin.width();
+                fraction += bin.numEntries()/bin.width();
+            }
+            else if((bin.xMin() < xmin) && (bin.xMax() > xmin))
+            {
+                double perc = (bin.xMax() - xmin)/bin.width();
+                integral += perc*(bin.sumW()/bin.width());
+                fraction += perc*(bin.numEntries()/bin.width());
+                
+            }
+            else if((bin.xMin() < xmax) && (bin.xMax() > xmax))
+            {
+                double perc = (xmax - bin.xMin())/bin.width();
+                integral += perc*(bin.sumW()/bin.width());
+                fraction += perc*(bin.numEntries()/bin.width());
+            }
+        }
+        
+        return integral;
+        
+    }
    
     /// Constructor
     DEFAULT_RIVET_ANALYSIS_CTOR(STAR_2006_I715470);
@@ -286,7 +362,9 @@ double highedge = 3.0*pi/2.0;
             c1.SetzTRange(0,1);
             Correlators.push_back(c1);
             string name = c1.GetCollSystemAndEnergy()+c1.GetFullIndex();
+            string name3 = c1.GetCollSystemAndEnergy()+c1.GetFullIndex()+"BkgdSubtracted";
             book(_h[name], name, ndPhiBins,lowedge,highedge);
+            book(_h[name3], name3, ndPhiBins,lowedge,highedge);
             book(sow[c1.GetFullIndex()],"sow" + c1.GetFullIndex());
             if(ncb==0){
 
@@ -298,7 +376,9 @@ double highedge = 3.0*pi/2.0;
               c2.SetzTRange(0,1); 
               Correlators.push_back(c2);
             string name2 = c2.GetCollSystemAndEnergy()+c2.GetFullIndex();
+            string name4 = c2.GetCollSystemAndEnergy()+c2.GetFullIndex()+"BkgdSubtracted";
             book(_h[name2], name2, ndPhiBins,lowedge,highedge);
+            book(_h[name4], name4, ndPhiBins,lowedge,highedge);
             book(sow[c2.GetFullIndex()],"sow" + c2.GetFullIndex());
             }
           }
@@ -317,7 +397,9 @@ double highedge = 3.0*pi/2.0;
             c1.SetAssociatedRange(3,15);
             Correlators.push_back(c1);
             string name = c1.GetCollSystemAndEnergy()+c1.GetFullIndex();
+            string name3 = c1.GetCollSystemAndEnergy()+c1.GetFullIndex()+"BkgdSubtracted";
             book(_h[name], name, ndPhiBins,lowedge,highedge);
+            book(_h[name3], name3, ndPhiBins,lowedge,highedge);
             book(sow[c1.GetFullIndex()],"sow" + c1.GetFullIndex());
             if(ncb==0){
 
@@ -329,7 +411,9 @@ double highedge = 3.0*pi/2.0;
               c2.SetAssociatedRange(3,15);
               Correlators.push_back(c2);
             string name2 = c2.GetCollSystemAndEnergy()+c2.GetFullIndex();
+            string name4 = c2.GetCollSystemAndEnergy()+c2.GetFullIndex()+"BkgdSubtracted";
             book(_h[name2], name2, ndPhiBins,lowedge,highedge);
+            book(_h[name4], name4, ndPhiBins,lowedge,highedge);
             book(sow[c2.GetFullIndex()],"sow" + c2.GetFullIndex());
             }
           }//End of the zT loop
@@ -504,6 +588,13 @@ double highedge = 3.0*pi/2.0;
             
             string name = corr.GetCollSystemAndEnergy()+corr.GetFullIndex();
             if(nTriggers[corr.GetFullIndex()] > 0) _h[name]->scaleW(sow[corr.GetFullIndex()]->numEntries()/(nTriggers[corr.GetFullIndex()]*sow[corr.GetFullIndex()]->sumW()));
+            string name2 = corr.GetCollSystemAndEnergy()+corr.GetFullIndex()+"BkgdSubtracted";
+                  _h[name2] = SubtractBackgroundZYAM(_h[name]);
+                  //You will need something like this
+
+  //            double fraction = 0.;
+    //        double yield = getYieldRangeUser(_h[name], M_PI-(M_PI/6.), M_PI+(M_PI/6.), fraction);
+      //      _h[nameFig12]->bin(corr.GetIndex()).fillBin(yield/fraction, fraction);
         }
         
         
