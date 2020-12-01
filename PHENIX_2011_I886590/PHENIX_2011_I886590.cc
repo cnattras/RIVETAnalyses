49e6c66962a4e66be3b73c5d974d0708c4ae5238
aschmier <aschmier@vols.utk.edu>
2020-11-30 20:52:52 -0500

    Booking histos and reformatting HEPdata with overlapping bins

--- PHENIX_2011_I886590/PHENIX_2011_I886590.cc
+++ PHENIX_2011_I886590/PHENIX_2011_I886590.cc
@@ -33,31 +33,51 @@ namespace Rivet {
       // The final-state particles declared above are clustered using FastJet with
       // the anti-kT algorithm and a jet-radius parameter 0.4
       // muons and neutrinos are excluded from the clustering
-      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
-      declare(jetfs, "jets");
+      //FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
+      //declare(jetfs, "jets");
 
       // FinalState of prompt photons and bare muons and electrons in the event
-      PromptFinalState photons(Cuts::abspid == PID::PHOTON);
-      PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
+      //PromptFinalState photons(Cuts::abspid == PID::PHOTON);
+      //PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
 
       // Dress the prompt bare leptons with prompt photons within dR < 0.1,
       // and apply some fiducial cuts on the dressed leptons
-      Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
-      DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
-      declare(dressed_leps, "leptons");
+      //Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
+      //DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
+      //declare(dressed_leps, "leptons");
 
       // Missing momentum
-      declare(MissingMomentum(fs), "MET");
+      //declare(MissingMomentum(fs), "MET");
 
       // Book histograms
       // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
+      //book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
+      //book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
+      //book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
       // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
+      
+      // Histos from HEPdata at 200GeV
+      book(_h["xsec_piplus_200"], 1, 1, 1);
+      book(_h["xsec_piminus_200"], 1, 1, 2);
+      book(_h["xsec_kplus_200"], 2, 1, 1);
+      book(_h["xsec_kminus_200"], 2, 1, 2);
+      book(_h["xsec_p_noFD_200_1"], 3, 1, 1);
+      book(_h["xsec_p_noFD_200_2"], 9, 1, 1);
+      //book(_h["xsec_pbar_noFD_200"], 3, 1, 2);
+      //book(_h["xsec_p_withFD_200"], 4, 1, 1);
+      //book(_h["xsec_pbar_withFD_200"], 4, 1, 2);
+
+      // Histos from HEPdata at 62.4GeV
+      book(_h["xsec_piplus_624"], 5, 1, 1);
+      book(_h["xsec_piminus_624"], 5, 1, 2);
+      book(_h["xsec_kplus_624"], 6, 1, 1);
+      book(_h["xsec_kminus_624"], 6, 1, 2);
+      //book(_h["xsec_p_noFD_624"], 7, 1, 1);
+      //book(_h["xsec_pbar_noFD_624"], 7, 1, 2);
+      //book(_h["xsec_p_withFD_624"], 8, 1, 1);
+      //book(_h["xsec_pbar_withFD_624"], 8, 1, 2);
+      //book(_p["BBBB"], 2, 1, 1);
+      //book(_c["CCCC"], 3, 1, 1);
 
     }
 
@@ -66,27 +86,27 @@ namespace Rivet {
     void analyze(const Event& event) {
 
       // Retrieve dressed leptons, sorted by pT
-      vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
+      //vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
 
       // Retrieve clustered jets, sorted by pT, with a minimum pT cut
-      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
+      //Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
 
       // Remove all jets within dR < 0.2 of a dressed lepton
-      idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
+      //idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
 
       // Select jets ghost-associated to B-hadrons with a certain fiducial selection
-      Jets bjets = filter_select(jets, [](const Jet& jet) {
-        return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
-      });
+      //Jets bjets = filter_select(jets, [](const Jet& jet) {
+        //return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
+      //});
 
       // Veto event if there are no b-jets
-      if (bjets.empty())  vetoEvent;
+      //if (bjets.empty())  vetoEvent;
 
       // Apply a missing-momentum cut
-      if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
+      //if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
 
       // Fill histogram with leading b-jet pT
-      _h["XXXX"]->fill(bjets[0].pT()/GeV);
+      //_h["XXXX"]->fill(bjets[0].pT()/GeV);
 
     }
 
@@ -94,9 +114,9 @@ namespace Rivet {
     /// Normalise histograms etc., after the run
     void finalize() {
 
-      normalize(_h["XXXX"]); // normalize to unity
-      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
+      //normalize(_h["XXXX"]); // normalize to unity
+      //normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
+      //scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
 
     }
 
@@ -108,6 +128,7 @@ namespace Rivet {
     map<string, Histo1DPtr> _h;
     map<string, Profile1DPtr> _p;
     map<string, CounterPtr> _c;
+    map<string, Scatter2DPtr> _s;
     //@}
 
 
--- PHENIX_2011_I886590/RunAnalysis.sh
+++ PHENIX_2011_I886590/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2011_I886590.so PHENIX_2011_I886590.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2011_I886590:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -a PHENIX_2011_I886590:beam=pp200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
