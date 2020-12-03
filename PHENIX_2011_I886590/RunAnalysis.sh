d938ce3835a02560ea38f59e0a6c1a1c7f7b2b51
aschmier <aschmier@vols.utk.edu>
2020-12-03 17:38:48 -0500

    Implementing beam options

--- PHENIX_2011_I886590/RunAnalysis.sh
+++ PHENIX_2011_I886590/RunAnalysis.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2011_I886590.so PHENIX_2011_I886590.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -a PHENIX_2011_I886590:beam=pp200 -o Rivet.yoda ../../sims/pp_200GeV/*
+rivet --pwd -a PHENIX_2011_I886590:beam=pp200 -o Rivet_pp200.yoda ../../sims/pp_200GeV/hepmc_pp_200GeV_1*.hepmc
+rivet --pwd -a PHENIX_2011_I886590:beam=pp62 -o Rivet_pp62.yoda ../../sims/pp_200GeV/hepmc_pp_200GeV_2*.hepmc
 #../testfiles/PYTHIAAuAuFileSMALLTEST.dat
 #../../sims/pp_200GeV/hepmc_pp_200GeV_28.hepmc
+
+rivet-mkhtml --pwd Rivet_pp200.yoda
+cp -r rivet-plots/ rivet-plots-pp200/
+
+rivet-mkhtml --pwd Rivet_pp62.yoda
+cp -r rivet-plots/ rivet-plots-pp62/
+
+
