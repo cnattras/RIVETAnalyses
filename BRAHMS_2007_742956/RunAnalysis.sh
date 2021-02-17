b9f98d0c18130c1d8f1533da47bbbbd083d22856
Manuel A. Rosales Aguilar <mrosales2016@rcas6011.rcf.bnl.gov>
2021-02-17 13:22:10 -0500

    Updated My Files Manny

--- BRAHMS_2007_742956/RunAnalysis.sh
+++ BRAHMS_2007_742956/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetBRAHMS_2007_I742956.so BRAHMS_2007_I742956.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a BRAHMS_2007_I742956:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -a BRAHMS_2007_I742956 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
