e0a14b8081a9f54c54b3250eceaca4c9899ada91
Manuel A. Rosales Aguilar <mrosales2016@rcas6016.rcf.bnl.gov>
2021-02-21 11:04:17 -0500

    Added histograms to my cc file

--- BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
+++ BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
@@ -30,63 +30,31 @@ namespace Rivet {
       // the given eta acceptance
       const FinalState fs(Cuts::abseta < 4.9);
 
-      // The final-state particles declared above are clustered using FastJet with
-      // the anti-kT algorithm and a jet-radius parameter 0.4
-      // muons and neutrinos are excluded from the clustering
-      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
-      declare(jetfs, "jets");
-
-      // FinalState of prompt photons and bare muons and electrons in the event
-      PromptFinalState photons(Cuts::abspid == PID::PHOTON);
-      PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
-
-      // Dress the prompt bare leptons with prompt photons within dR < 0.1,
-      // and apply some fiducial cuts on the dressed leptons
-      Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
-      DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
-      declare(dressed_leps, "leptons");
-
-      // Missing momentum
-      declare(MissingMomentum(fs), "MET");
-
       // Book histograms
       // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
-      // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
-
-    }
+      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);	
+      book(_h["CrsSecPI+"], 1, 1, 1);
+      book(_h["CrsSecPI-"], 2, 1, 1);
+      book(_h["CrsSecK+"], 3, 1, 1);
+      book(_h["CrsSecP"], 4, 1, 1);
+      book(_h["CrsSecAntP"], 5, 1, 1);
+      book(_h["CrsSecK+"], 6, 1, 1);
+//Rapidity Changes at this point
+      book(_h["CrsSecPI+R2"], 7, 1, 1);
+      book(_h["CrsSecPI-R2"], 8, 1, 1);
+      book(_h["CrsSecK+R2"], 9, 1, 1);
+      book(_h["CrsSecPR2"], 10, 1, 1);
+      book(_h["CrsSecAntPR2"], 11, 1, 1);
+      book(_h["CrsSecK+R2"], 12, 1, 1);
+ }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
 
-      // Retrieve dressed leptons, sorted by pT
-      vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
-
-      // Retrieve clustered jets, sorted by pT, with a minimum pT cut
-      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
-
-      // Remove all jets within dR < 0.2 of a dressed lepton
-      idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
-
-      // Select jets ghost-associated to B-hadrons with a certain fiducial selection
-      Jets bjets = filter_select(jets, [](const Jet& jet) {
-        return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
-      });
-
-      // Veto event if there are no b-jets
-      if (bjets.empty())  vetoEvent;
-
-      // Apply a missing-momentum cut
-      if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
 
       // Fill histogram with leading b-jet pT
-      _h["XXXX"]->fill(bjets[0].pT()/GeV);
+      //_h["XXXX"]->fill(bjets[0].pT()/GeV);
 
     }
 
@@ -94,17 +62,15 @@ namespace Rivet {
     /// Normalise histograms etc., after the run
     void finalize() {
 
-      normalize(_h["XXXX"]); // normalize to unity
-      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
+      //normalize(_h["XXXX"]); // normalize to unity
 
     }
 
     //@}
 
 
-    /// @name Histograms
-    //@{
+    //@name Histograms
+    // @{
     map<string, Histo1DPtr> _h;
     map<string, Profile1DPtr> _p;
     map<string, CounterPtr> _c;
