611451a6af186a7c7c51370825783785da310396
antoniocosilva <antonio.ifusp@gmail.com>
2021-03-17 16:20:26 -0400

    Added comments

--- FIFO/RunFIFO.sh
+++ FIFO/RunFIFO.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Copy the main113 file to the same directory where you have your Rivet Analysis
+#On ACF you can find main113 here: /lustre/haven/proj/UTK0019/FIFO
+#Change the name "PHENIX_2008_I778168" to your Rivet Analysis
+#Have your centrality calibration file in the same folder or point $CALIBRATION to the right path/file
 ANALYSIS="PHENIX_2008_I778168"
 ANALYSIS_DIR=$PWD
 source /lustre/haven/proj/UTK0019/Rivet3.1.3/local/rivetenv.sh
@@ -17,8 +21,10 @@ GENERATOR_SEED="0"
 #Min and max pT-hard. if PTHARDMIN > PTHARDMAX then pythia switches to minimum bias (No pt-hard)
 PTHARDMIN="0"
 PTHARDMAX="-1"
+#Centrality Calibration file
+CALIBRATION="calibration_PHENIX_AuAu62GeV.yoda"
 #Flags of your analysis (Ex. centrality: cent=GEN)
 RIVET_FLAGS=":cent=GEN"
 rm /tmp/$FIFOFILE
 mkfifo /tmp/$FIFOFILE
-./main113 /tmp $FIFOFILE $NEVENTS $BEAM1 $BEAM2 $CMS_ENERGY $GENERATOR_SEED $PTHARDMIN $PTHARDMAX & rivet --pwd -p calibration_PHENIX_AuAu62GeV.yoda -a $ANALYSIS$RIVET_FLAGS -o Rivet.yoda /tmp/$FIFOFILE
+./main113 /tmp $FIFOFILE $NEVENTS $BEAM1 $BEAM2 $CMS_ENERGY $GENERATOR_SEED $PTHARDMIN $PTHARDMAX & rivet --pwd -p $CALIBRATION -a $ANALYSIS$RIVET_FLAGS -o Rivet.yoda /tmp/$FIFOFILE
