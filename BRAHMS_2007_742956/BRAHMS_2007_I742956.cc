5226c0efb45f77c3d1988891e1d703e6f07dda8e
Manuel A. Rosales Aguilar <mrosales2016@rcas6011.rcf.bnl.gov>
2021-03-17 15:11:41 -0400

    Updating my macro

--- BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
+++ BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
@@ -37,9 +37,23 @@ namespace Rivet {
           // specify custom binning
         vector<double> pTedges{0.35, 0.4, 0.45, 0.5, 0.55, .6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 3.0, 3.4, 3.8, 4.2, 5.2};
 
-        book(_h["ProtonPt"], "ProtonPt", pTedges);
+        //First rapidity y = 2.95
+        book(_h["CSPi+Pt"], "CSPi+Pt", pTedges);
+        book(_h["CSPi-Pt"], "CSPi-Pt", pTedges);
+        book(_h["CSK+Pt"], "CSK+Pt", pTedges);
+        book(_h["CSK-Pt"], "CSK-Pt", pTedges);
+        book(_h["CSPPt"], "CSPPt", pTedges);
+        book(_h["CSAntiPPt"], "CSAntiPPt", pTedges);
         
-        //Hitrograms below are from the actual data
+        book(_h["CSPi+Pt2"], "CSPi+Pt2", pTedges);
+        book(_h["CSPi-Pt2"], "CSPi-Pt2", pTedges);
+        book(_h["CSK+Pt2"], "CSK+Pt2", pTedges);
+        book(_h["CSK-Pt2"], "CSK-Pt2", pTedges);
+        book(_h["CSPPt2"], "CSPPt2", pTedges);
+        book(_h["CSAntiPPt2"], "CSAntiPPt2", pTedges);
+    
+        
+        //Second rapidity y =3.3
         book(_h["CrsSecPI+"], 1, 1, 1);
         book(_h["CrsSecPI-"], 2, 1, 1);
         book(_h["CrsSecK+"], 3, 1, 1);
@@ -62,12 +76,30 @@ namespace Rivet {
 
         Particles pfsParticles = applyProjection<FinalState>(event,"pfs").particles();
 
-        for(const Particle& p : pfsParticles)
+        for(const Particle& p : pfsParticles) //Particle Looping
         {
-            if(p.pid() == 2212) _h["ProtonPt"]->fill(p.pT()/GeV);
+            if((abs(p.rapidity())/GeV<3.0)&&(abs(p.rapidity())/GeV<2.9)){ // Interested in regions near 2.95
+                if(p.pid() == 211) _h["CSPi+Pt"]->fill(p.pT()/GeV); //Pion
+                if(p.pid() == -211) _h["CSPi-Pt"]->fill(p.pT()/GeV);
+                
+                if(p.pid() == 321) _h["CSK+Pt"]->fill(p.pT()/GeV); //Kaon
+                if(p.pid() == -321) _h["CSK-Pt"]->fill(p.pT()/GeV);
+                
+                if(p.pid() == 2212) _h["CSPPt"]->fill(p.pT()/GeV); //Pion
+                if(p.pid() == -2212) _h["CSAntiPPt"]->fill(p.pT()/GeV);
+            }
+            if((abs(p.rapidity())/GeV<3.35)&&(abs(p.rapidity())/GeV<3.25)){ // Interested in regions near 2.95
+                if(p.pid() == 211) _h["CSPi+Pt2"]->fill(p.pT()/GeV); //Pion
+                if(p.pid() == -211) _h["CSPi-Pt2"]->fill(p.pT()/GeV);
+                
+                if(p.pid() == 321) _h["CSK+Pt2"]->fill(p.pT()/GeV); //Kaon
+                if(p.pid() == -321) _h["CSK-Pt2"]->fill(p.pT()/GeV);
+                
+                if(p.pid() == 2212) _h["CSPPt2"]->fill(p.pT()/GeV); //Pion
+                if(p.pid() == -2212) _h["CSAntiPPt2"]->fill(p.pT()/GeV);
+            }
+            
         }
-        
-////
 
     }
 
