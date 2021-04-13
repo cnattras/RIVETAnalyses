32d5a765795aa09526ad8f72e9389e967474b067
Manuel A. Rosales Aguilar <mrosales2016@rcas6016.rcf.bnl.gov>
2021-04-13 10:49:02 -0400

    Updating CC file

--- BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
+++ BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
@@ -5,6 +5,7 @@
 #include "Rivet/Projections/DressedLeptons.hh"
 #include "Rivet/Projections/MissingMomentum.hh"
 #include "Rivet/Projections/PromptFinalState.hh"
+#include "math.h"
 
 namespace Rivet {
 
@@ -30,31 +31,9 @@ namespace Rivet {
       // the given eta acceptance
        
         const FinalState fs(Cuts::abseta < 5);//The cut is for particles we want thus <5 correponds to particles with 5 GeV or less.
-//        const PromptFinalState fs(Cuts::pT > 5);
         declare(fs, "fs");
-
-
-        vector<double> pTedges{0.35, 0.4, 0.45, 0.5, 0.55, .6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 3.0, 3.4, 3.8, 4.2, 5.2};
         
-        book(_h["PtDist"], "pTDist", 100, 0.0, 5.0);
-
-//        First rapidity y = 2.95
-        book(_h["CSPiplus"], "CSPiplus", pTedges);
-        book(_h["CSPiminus"], "CSPiminus", pTedges);
-        book(_h["CSKplus"], "CSKplus", pTedges);
-        book(_h["CSKminus"], "CSKminus", pTedges);
-        book(_h["CSPPt"], "CSPPt", pTedges);
-        book(_h["CSAntiPPt"], "CSAntiPPt", pTedges);
-        
-//        //Second rapidity y =3.3
-        book(_h["CSPiplus2"], "CSPiplus2", pTedges);
-        book(_h["CSPiminus2"], "CSPiminus2", pTedges);
-        book(_h["CSKplus2"], "CSKplus2", pTedges);
-        book(_h["CSKminus2"], "CSKminus2", pTedges);
-        book(_h["CSPPt2"], "CSPPt2", pTedges);
-        book(_h["CSAntiPPt2"], "CSAntiPPt2", pTedges);
-
-        
+    
         book(_h["CrsSecPIplus"], 1, 1, 1);
         book(_h["CrsSecPIminus"], 2, 1, 1);
         book(_h["CrsSecKplus"], 3, 1, 1);
@@ -68,7 +47,8 @@ namespace Rivet {
         book(_h["CrsSecKminus2"], 10, 1, 1);
         book(_h["CrsSecP2"], 11, 1, 1);
         book(_h["CrsSecAntiP2"], 12, 1, 1);
-
+        
+        
     }
 
 
@@ -80,46 +60,54 @@ namespace Rivet {
         for(const Particle& p : fsParticles)   //Particle Looping
         {
 
+            double ySize = .1;
             
-            double partPt = p.pT() / GeV;
+            if((abs(p.rapidity())<3.0)&&(abs(p.rapidity())<2.9)){ // Interested in regions near 2.95
+                if(p.pid() == 211) _h["CrsSecPIplus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
+                if(p.pid() == -211) _h["CrsSecPIminus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
             
-            _h["PtDist"]->fill(partPt);
+                if(p.pid() == 321) _h["CrsSecKplus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Kaon
+                if(p.pid() == -321) _h["CrsSecKminus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
             
-            if((abs(p.rapidity())/GeV<3.0)&&(abs(p.rapidity())/GeV<2.9)){ // Interested in regions near 2.95
-                if(p.pid() == 211) _h["CSPiplus"]->fill(p.pT()/GeV); //Pion
-                if(p.pid() == -211) _h["CSPiminus"]->fill(p.pT()/GeV);
-
-                if(p.pid() == 321) _h["CSKplus"]->fill(p.pT()/GeV); //Kaon
-                if(p.pid() == -321) _h["CSKminus"]->fill(p.pT()/GeV);
-
-                if(p.pid() == 2212) _h["CSPPt"]->fill(p.pT()/GeV); //Pion
-                if(p.pid() == -2212) _h["CSAntiPPt"]->fill(p.pT()/GeV);
+                if(p.pid() == 2212) _h["CrsSecP"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
+                if(p.pid() == -2212) _h["CrsSecAntiP"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
             }
-            if((abs(p.rapidity())/GeV<3.35)&&(abs(p.rapidity())/GeV<3.25)){ // Interested in regions near 2.95
-                if(p.pid() == 211) _h["CSPiplus2"]->fill(p.pT()/GeV); //Pion
-                if(p.pid() == -211) _h["CSPiminus2"]->fill(p.pT()/GeV);
-
-                if(p.pid() == 321) _h["CSKplus2"]->fill(p.pT()/GeV); //Kaon
-                if(p.pid() == -321) _h["CSKminus2"]->fill(p.pT()/GeV);
-
-                if(p.pid() == 2212) _h["CSPPt2"]->fill(p.pT()/GeV); //Pion
-                if(p.pid() == -2212) _h["CSAntiPPt2"]->fill(p.pT()/GeV);
-            }
+            if((abs(p.rapidity())<3.35)&&(abs(p.rapidity())<3.25)){ // Interested in regions near 2.95
+                if(p.pid() == 211) _h["CrsSecPIplus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
+                if(p.pid() == -211) _h["CrsSecPIminus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
             
+                if(p.pid() == 321) _h["CrsSecKplus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Kaon
+                if(p.pid() == -321) _h["CrsSecKminus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+            
+                if(p.pid() == 2212) _h["CrsSecP2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
+                if(p.pid() == -2212) _h["CrsSecAntiP2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+            }
+        
         }
+        
 
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-//
-//      normalize(_h["PtDist"]); // normalize to unity
-//      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-//      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
-
+        
+        scale(_h["CrsSecPIplus"], ((crossSection()*pow(10,-6))/sumOfWeights())); // Still missing mean pT, pTbinsize and general nEvents, 1/Lum, also what does scale do?
+        scale(_h["CrsSecPIminus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecKplus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecKminus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecP"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecAntiP"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecPIplus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecPIminus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecKplus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecKminus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecP2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        scale(_h["CrsSecAntiP2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
+        
+        
     }
-
+ 
     //@}
 
 
@@ -137,3 +125,16 @@ namespace Rivet {
   DECLARE_RIVET_PLUGIN(BRAHMS_2007_I742956);
 
 }
+
+//        _h["CrsSecPIplus"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecPIminus"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecKplus"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecKminus"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecP"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecAntiP"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecPIplus2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecPIminus2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecKplus2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecKminus2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecP2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
+//        _h["CrsSecAntiP2"]->scaleW((crossSection()/picobarn)/_c["sow"]->sumW());
