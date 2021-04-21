99dc36ffea00e435754080c1ec4f6f5500412b3e
Manuel A. Rosales Aguilar <mrosales2016@rcas6006.rcf.bnl.gov>
2021-04-21 13:42:43 -0400

    Uploading Macro File

--- BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
+++ BRAHMS_2007_742956/BRAHMS_2007_I742956.cc
@@ -23,16 +23,9 @@ namespace Rivet {
 
     /// Book histograms and initialise projections before the run
     void init() {
-
-      // Initialise and register projections
-
-      // The basic final-state projection:
-      // all final-state particles within
-      // the given eta acceptance
        
-        const FinalState fs(Cuts::abseta < 5);//The cut is for particles we want thus <5 correponds to particles with 5 GeV or less.
+        const FinalState fs(Cuts::abseta < 5);
         declare(fs, "fs");
-        
     
         book(_h["CrsSecPIplus"], 1, 1, 1);
         book(_h["CrsSecPIminus"], 2, 1, 1);
@@ -55,83 +48,57 @@ namespace Rivet {
     /// Perform the per-event analysis
     void analyze(const Event& event) {
 
-        Particles fsParticles = applyProjection<FinalState>(event,"fs").particles();
+        Particles fsParticles = applyProjection<FinalState>(event,"fs").particles(); //At some point we will want to look at primary vs final state particles instead. The problem is BHRAMS experiment did no feed down correction.
 
-        for(const Particle& p : fsParticles)   //Particle Looping
+        for(const Particle& p : fsParticles)
         {
 
-            double ySize = .1;
+            double ySize = .1; //Looking at only forward Rapdities
             
-            if((abs(p.rapidity())<3.0)&&(abs(p.rapidity())<2.9)){ // Interested in regions near 2.95
-                if(p.pid() == 211) _h["CrsSecPIplus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
-                if(p.pid() == -211) _h["CrsSecPIminus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+            double pT = p.pT() / GeV;
             
-                if(p.pid() == 321) _h["CrsSecKplus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Kaon
-                if(p.pid() == -321) _h["CrsSecKminus"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+            if((p.rapidity()>2.9)&&(p.rapidity()<3.0)){ // Interested in regions near 2.95
+                if(p.pid() == 211) _h["CrsSecPIplus"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Pion
+                if(p.pid() == -211) _h["CrsSecPIminus"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
             
-                if(p.pid() == 2212) _h["CrsSecP"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
-                if(p.pid() == -2212) _h["CrsSecAntiP"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+                if(p.pid() == 321) _h["CrsSecKplus"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Kaon
+                if(p.pid() == -321) _h["CrsSecKminus"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
+            
+                if(p.pid() == 2212) _h["CrsSecP"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Pion
+                if(p.pid() == -2212) _h["CrsSecAntiP"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
             }
-            if((abs(p.rapidity())<3.35)&&(abs(p.rapidity())<3.25)){ // Interested in regions near 2.95
-                if(p.pid() == 211) _h["CrsSecPIplus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
-                if(p.pid() == -211) _h["CrsSecPIminus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
-<<<<<<< HEAD
+            if((p.rapidity()>3.25)&&(p.rapidity()<3.35)){ // Interested in regions near 3.3
+                if(p.pid() == 211) _h["CrsSecPIplus2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Pion
+                if(p.pid() == -211) _h["CrsSecPIminus2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
             
-                if(p.pid() == 321) _h["CrsSecKplus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Kaon
-                if(p.pid() == -321) _h["CrsSecKminus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+                if(p.pid() == 321) _h["CrsSecKplus2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Kaon
+                if(p.pid() == -321) _h["CrsSecKminus2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
             
-=======
-            
-                if(p.pid() == 321) _h["CrsSecKplus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Kaon
-                if(p.pid() == -321) _h["CrsSecKminus2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
-            
->>>>>>> dae383376ddc13a159f6cd722bd477cfb9cd81d7
-                if(p.pid() == 2212) _h["CrsSecP2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize)); //Pion
-                if(p.pid() == -2212) _h["CrsSecAntiP2"]->fill(p.pT()/GeV,(1/p.pT())*(1/(2*pi))*(1/ySize));
+                if(p.pid() == 2212) _h["CrsSecP2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize)); //Pion
+                if(p.pid() == -2212) _h["CrsSecAntiP2"]->fill(pT,(1.0/pT)*(1.0/(2.0*pi))*(1.0/ySize));
             }
         
         }
         
-
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-<<<<<<< HEAD
         
-        scale(_h["CrsSecPIplus"], ((crossSection()*pow(10,-6))/sumOfWeights())); // Still missing mean pT, pTbinsize and general nEvents, 1/Lum, also what does scale do?
-        scale(_h["CrsSecPIminus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecKplus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecKminus"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecP"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecAntiP"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecPIplus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecPIminus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecKplus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecKminus2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecP2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-        scale(_h["CrsSecAntiP2"], ((crossSection()*pow(10,-6))/sumOfWeights()));
-=======
-//
-//      normalize(_h["PtDist"]); // normalize to unity
-//      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-//
-//        scale(_h["CSPiplus"], (crossSection()/picobarn/sumOfWeights())); // Still missing mean pT, pTbinsize and general nEvents, 1/Lum, also what does scale do?
-//        scale(_h["CSPiminus"],(crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSKplus"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSKminus"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSPPt"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSAntiPPt"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSPiplus2"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSPiminus2"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSKplus2"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSKminus2"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSPPt2"], (crossSection()/picobarn/sumOfWeights()));
-//        scale(_h["CSAntiPPt2"], (crossSection()/picobarn/sumOfWeights()));
->>>>>>> dae383376ddc13a159f6cd722bd477cfb9cd81d7
-        
-        
+        scale(_h["CrsSecPIplus"], ((crossSection()/millibarn)/sumOfWeights())); // XSec is in units of mb in paper. Typicall run prints out in picobarns
+        scale(_h["CrsSecPIminus"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecKplus"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecKminus"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecP"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecAntiP"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecPIplus2"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecPIminus2"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecKplus2"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecKminus2"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecP2"], ((crossSection()/millibarn)/sumOfWeights()));
+        scale(_h["CrsSecAntiP2"], ((crossSection()/millibarn)/sumOfWeights()));
+                
     }
  
     //@}
@@ -139,7 +106,7 @@ namespace Rivet {
 
     /// @name Histograms
     //@{
-    map<string, Histo1DPtr> _h;
+    map<string, Histo1DPtr> _h; //Mapping concern
     map<string, Profile1DPtr> _p;
     map<string, CounterPtr> _c;
     //@}
