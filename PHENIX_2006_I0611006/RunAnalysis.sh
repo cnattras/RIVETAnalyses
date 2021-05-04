56235ab288076b76fdefb377c9b0b6be4d4883c6
cnattras <saccharomyces.cerevisae@gmail.com>
2021-05-04 15:07:36 -0400

    tweaks

--- PHENIX_2006_I0611006/RunAnalysis.sh
+++ PHENIX_2006_I0611006/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-buildplugin RivetPHENIX_2006_I0611006.so PHENIX_2006_I0611006.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I0611006:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda hepmc_AuAu_200GeV_1.hepmc.gz
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I0611006:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
