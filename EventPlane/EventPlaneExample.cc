bc9323acdc3eb77a545c24c14444498a87c34389
antoniocosilva <antonio.ifusp@gmail.com>
2021-05-05 08:23:23 -0400

    Changed atan2 to atan in GetEventPlaneDetectorAcc()

--- EventPlane/EventPlaneExample.cc
+++ EventPlane/EventPlaneExample.cc
@@ -74,7 +74,7 @@ namespace Rivet {
                     }
             }
 
-            double eventPlane = mapAngle0To2Pi((1./n)*atan2(QIn,QRn));
+            double eventPlane = mapAngle0To2Pi((1./n)*atan(QIn,QRn));
 
             return eventPlane;
     }
