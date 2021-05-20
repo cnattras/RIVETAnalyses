e0e0cf47c6aef478f30001152bea08dc91323459
Christal Martin <cbaillar@acf-login6.acf.nics.utk.edu>
2021-05-20 12:33:54 -0400

    clean up

---
+++ 1107.2955/RunAnalysis.sh
@@ -0,0 +1,6 @@
+
+#This compiles the code
+rivet-build RivetSTAR_2012_I918779.so STAR_2012_I918779.cc
+#This runs it
+rivet --pwd -p ../Centralities/Calibration/calibration_STAR_AuAu200GeV.yoda -a STAR_2012_I918779:cent=GEN:beam=AUAU -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+#rivet-mkhtml --pwd Rivet.yoda
