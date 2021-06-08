3eb499787a22772974191eaa01128c612edeeb1b
cnattras <saccharomyces.cerevisae@gmail.com>
2021-06-08 14:29:30 -0400

    updating test script

--- PHENIX_2008_I776624/RunAnalysis.sh
+++ PHENIX_2008_I776624/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2008_I776624.so PHENIX_2008_I776624.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2008_I776624:cent=GEN: -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p calibration_PHENIX_CuCu200GeV.yoda -a PHENIX_2008_I776624:cent=GEN:beam=CUCU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
