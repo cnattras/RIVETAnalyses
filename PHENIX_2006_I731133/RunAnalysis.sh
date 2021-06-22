b9a78ca833802ca208dd04d92652817ff0fbb434
Adam C. Tilley <atilley2@acf-login7.acf.nics.utk.edu>
2021-06-22 12:16:50 -0400

    Trying to fix my repository

--- PHENIX_2006_I731133/RunAnalysis.sh
+++ PHENIX_2006_I731133/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rivet-buildplugin RivetPHENIX_2006_I0611006.so PHENIX_2006_I0611006.cc
+rivet-build RivetPHENIX_2006_I731133.so PHENIX_2006_I731133.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I0611006:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I731133:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
