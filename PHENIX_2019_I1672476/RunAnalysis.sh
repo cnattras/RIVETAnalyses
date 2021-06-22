e54c1ea671bb9c73d00155a3a4c8753c16c1c379
Nikolas Kip Hunt <nhunt5@acf-login5.acf.nics.utk.edu>
2021-06-22 10:43:29 -0400

    took out beam flag option

--- PHENIX_2019_I1672476/RunAnalysis.sh
+++ PHENIX_2019_I1672476/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2019_I1672476.so PHENIX_2019_I1672476.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2019_I1672476:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2019_I1672476:cent=GEN -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
