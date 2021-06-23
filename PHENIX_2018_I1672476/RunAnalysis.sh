940371b244d901a1ab547029b171a67dacda167e
Christal Martin <cbaillar@acf-login7.acf.nics.utk.edu>
2021-06-23 07:51:30 -0400

    removing extra file for analysis with incorrect year

--- PHENIX_2018_I1672476/RunAnalysis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm RivetPHENIX_2018_I1672476.so
-rivet-build RivetPHENIX_2018_I1672476.so PHENIX_2018_I1672476.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2018_I1672476:cent=GEN -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
