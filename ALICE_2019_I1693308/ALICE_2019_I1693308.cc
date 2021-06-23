062c6c9430feaab5ba1577ddb7b34e4691e568fd
Justin Daniel Piel <jpiel@lxplus705.cern.ch>
2021-06-24 00:15:02 +0200

    Working on Analysis

--- ALICE_2019_I1693308/ALICE_2019_I1693308.cc
+++ ALICE_2019_I1693308/ALICE_2019_I1693308.cc
@@ -30,8 +30,8 @@ namespace Rivet {
 
       book(_h["PPS7"],1,1,1);
       book(_h["pTJ5-10"],2,1,1);
-      book(_h["CCCC"],3,1,1);
-      book(_h["DDDD"],4,1,1);
+      book(_h["pTJ10-15"],3,1,1);
+      book(_h["pTJ15-20"],4,1,1);
 
       book(_c["sow"], "sow");
       book(_c["sow2"],"sow2");
@@ -48,41 +48,57 @@ namespace Rivet {
       const FastJets jetsfs = apply<FastJets>(event, "jetsfs");
 
       Jets jets = jetsfs.jetsByPt(Cuts::abseta < 0.5 && Cuts::pT >= 5.*GeV);
-      Particles fsParticles = applyProjection<FinalState>(event,"fs").particles();
+      //Particles fsParticles = applyProjection<FinalState>(event,"fs").particles();
+      _c["sow"]->fill();
 
       for(auto jet : jets)
       {
         _h["PPS7"]->fill(jet.pT()/GeV);
-
-        for(const Particle&p:fsParticles){
         if(jet.pT() >= 5*GeV && jet.pT() < 10*GeV){
-          _h["pTJ5-10"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
           _c["sow2"]->fill();
-          //_h["pTJ5-10"]->fill(jet.pT()/GeV);
+
         }else if(jet.pT() >= 10*GeV && jet.pT() < 15*GeV){
-          _h["CCCC"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
           _c["sow3"]->fill();
-          //_h["CCCC"]->fill(jet.pT()/GeV);
+
         } else if(jet.pT() >= 15*GeV && jet.pT() < 20*GeV){
-          _h["DDDD"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
           _c["sow4"]->fill();
-          //_h["DDDD"]->fill(jet.pT()/GeV);
+
         }
-        //_c["sow2"]->fill();
-      }
+        for(const Particle&p:jet.particles()){
+        if(jet.pT() >= 5*GeV && jet.pT() < 10*GeV){
+
+          _h["pTJ5-10"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
+        }else if(jet.pT() >= 10*GeV && jet.pT() < 15*GeV){
+
+          _h["pTJ10-15"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
+        } else if(jet.pT() >= 15*GeV && jet.pT() < 20*GeV){
+
+          _h["pTJ15-20"]->fill((p.pT()/GeV)/(jet.pT()/GeV));
+
+        }
       }
 
-      _c["sow"]->fill();
+      }
+
 
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-      _h["PPS7"]->scaleW(1./_c["sow"]->sumW());
-      _h["pTJ5-10"]->scaleW(1./_c["sow2"]->sumW());
-      _h["CCCC"]->scaleW(1./_c["sow3"]->sumW());
-      _h["DDDD"]->scaleW(1./_c["sow4"]->sumW());
+
+      double norm = (crossSection()*1.E-9)/sow->sumW();
+
+      _h["PPS7"]->scaleW(norm);
+
+      _h["pTJ5-10"]->scaleW(1./_c["sow"]->sumW()*(_c["sow"]->effNumEntries()/_c["sow2"]->effNumEntries()));
+      _h["pTJ10-15"]->scaleW(1./_c["sow"]->sumW()*(_c["sow"]->effNumEntries()/_c["sow3"]->effNumEntries()));
+      _h["pTJ15-20"]->scaleW(1./_c["sow"]->sumW()*(_c["sow"]->effNumEntries()/_c["sow4"]->effNumEntries()));
 
     }
 
