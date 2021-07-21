50fe16cff6403805c9844921186dd103db361c71
antoniocosilva <antonio.ifusp@gmail.com>
2021-07-20 20:18:14 -0400

    Changed main113 to runPythia

--- FIFO/RunFIFO.sh
+++ FIFO/RunFIFO.sh
@@ -2,7 +2,7 @@
 #ATTENTION! FIFO requires a lot of virtual memory. When entering in debug more, use: qsub -I -l nodes=1:ppn=4 -q debug
 #This will request 4 cores and sufficient virtual memory
 #Copy the main113 file to the same directory where you have your Rivet Analysis
-#On ACF you can find main113 here: /lustre/haven/proj/UTK0019/FIFO
+#On ACF you can find runPythia here: /lustre/haven/proj/UTK0019/Rivet
 #Change the name "PHENIX_2008_I778168" to your Rivet Analysis
 #Have your centrality calibration file in the same folder or point $CALIBRATION to the right path/file
 ANALYSIS="PHENIX_2008_I778168"
@@ -29,4 +29,4 @@ CALIBRATION="calibration_PHENIX_AuAu62GeV.yoda"
 RIVET_FLAGS=":cent=GEN"
 rm /tmp/$FIFOFILE
 mkfifo /tmp/$FIFOFILE
-./main113 /tmp $FIFOFILE $NEVENTS $BEAM1 $BEAM2 $CMS_ENERGY $GENERATOR_SEED $PTHARDMIN $PTHARDMAX & rivet --pwd -p $CALIBRATION -a $ANALYSIS$RIVET_FLAGS -o Rivet.yoda /tmp/$FIFOFILE
+./runPythia /tmp $FIFOFILE $NEVENTS $BEAM1 $BEAM2 $CMS_ENERGY $GENERATOR_SEED $PTHARDMIN $PTHARDMAX & rivet --pwd -p $CALIBRATION -a $ANALYSIS$RIVET_FLAGS -o Rivet.yoda /tmp/$FIFOFILE
