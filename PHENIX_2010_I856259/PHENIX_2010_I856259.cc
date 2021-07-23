58d1d9069607ceeec89b08ad58a57a777a32def6
Adam C. Tilley <atilley2@acf-login6.acf.nics.utk.edu>
2021-07-23 14:06:08 -0400

    updating cc file

--- PHENIX_2010_I856259/PHENIX_2010_I856259.cc
+++ PHENIX_2010_I856259/PHENIX_2010_I856259.cc
@@ -1,10 +1,5 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
-#include "Rivet/Projections/FinalState.hh"
-#include "Rivet/Projections/FastJets.hh"
-#include "Rivet/Projections/DressedLeptons.hh"
-#include "Rivet/Projections/MissingMomentum.hh"
-#include "Rivet/Projections/PromptFinalState.hh"
 #include "Rivet/Projections/UnstableParticles.hh"
 #include "../Centralities/RHICCentrality.hh"
 #include <math.h>
@@ -29,11 +24,25 @@ namespace Rivet {
 			
 			std::initializer_list<int> pdgIds = {221, 111};
 
-			const UnstableParticles np(pdgIds, Cuts::abseta < .35);
-			declare(np, "np");
+			const UnstableParticles up(pdgIds, Cuts::abseta < .35);
+			declare(up, "up");
 
 			declareCentrality(RHICCentrality("PHENIX"), "RHIC_2019_CentralityCalibration:exp=PHENIX", "CMULT", "CMULT");
 		
+			//____Counters____
+			
+			book(sow["sow_AUAU0_5"],"sow_AUAU0_5");
+			book(sow["sow_AUAU0_10"],"sow_AUAU0_10");
+			book(sow["sow_AUAU10_20"],"sow_AUAU10_20");
+			book(sow["sow_AUAU0_20"],"sow_AUAU0_20");
+			book(sow["sow_AUAU20_40"],"sow_AUAU20_40");
+			book(sow["sow_AUAU40_60"],"sow_AUAU40_60");
+			book(sow["sow_AUAU20_60"],"sow_AUAU40_60");
+			book(sow["sow_AUAU60_92"],"sow_AUAU60_92");
+			book(sow["sow_AUAU0_92"],"sow_AUAU0_92");
+			book(sow["sow_PP"],"sow_PP");
+
+
 			//____Cross Section vs. pT____
 
 			book(AUAU_cross["cross0_5"],1,1,1);
@@ -54,61 +63,61 @@ namespace Rivet {
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_0_5Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_0_5Eta", refdataRaaEta);
-			book(sRaa{"RaaEta0_5"], refnameRaaEta);
+			book(sRaa["RaaEta0_5"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(12,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_0_10Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_0_10Eta", refdataRaaEta);
-			book(sRaa{"RaaEta0_10"], refnameRaaEta);
+			book(sRaa["RaaEta0_10"], refnameRaaEta);
 	
 			string refnameRaaEta = mkAxisCode(13,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_10_20Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_10_20Eta", refdataRaaEta);
-			book(sRaa{"RaaEta10_20"], refnameRaaEta);
+			book(sRaa["RaaEta10_20"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(14,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_0_20Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_0_20Eta", refdataRaaEta);
-			book(sRaa{"RaaEta0_20"], refnameRaaEta);
+			book(sRaa["RaaEta0_20"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(15,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_20_40Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_20_40Eta", refdataRaaEta);
-			book(sRaa{"RaaEta20_40"], refnameRaaEta);
+			book(sRaa["RaaEta20_40"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(16,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_40_60Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_40_60Eta", refdataRaaEta);
-			book(sRaa{"RaaEta40_60"], refnameRaaEta);
+			book(sRaa["RaaEta40_60"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(17,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_20_60Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_20_60Eta", refdataRaaEta);
-			book(sRaa{"RaaEta20_60"], refnameRaaEta);
+			book(sRaa["RaaEta20_60"], refnameRaaEta);
 
 			string refnameRaaEta = mkAxisCode(18,1,1);
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_60_92Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_60_92Eta", refdataRaaEta);
-			book(sRaa{"RaaEta60_92"], refnameRaaEta);
+			book(sRaa["RaaEta60_92"], refnameRaaEta);
 
-			string refnameRaaEta = mkAxisCode(19,1,1);
+			string refnameRaaEta = mkAxisCode(19,1,1);	//Figures 3-9 and 4-2 in HEPData; should be used twice in finalize
 			const Scatter2D& refdataRaaEta = refData(refnameRaaEta);
 			book(hEta["AUAUyield"], refnameRaaEta + "_0_92Eta", refdataRaaEta);
 			book(hEta["AUAUcross"], refnameRaaEta + "_0_92Eta", refdataRaaEta);
-			book(sRaa{"RaaEta0_92"], refnameRaaEta);
+			book(sRaa["RaaEta0_92"], refnameRaaEta);
 
 			string refnameRaaPi = mkAxisCode(20,1,1);
 			const Scatter2D& refdataRaaPi = refData(refnameRaaPi);
 			book(hPi["AUAUyield"], refnameRaaPi + "_0_92Pi", refdataRaaPi);
 			book(hPi["AUAUcross"], refnameRaaPi + "_0_92Pi", refdataRaaPi);
-			book(sRaa{"RaaPi0_92"], refnameRaaPi);
+			book(sRaa["RaaPi0_92"], refnameRaaPi);
 
 
 
@@ -120,12 +129,16 @@ namespace Rivet {
 		/// Perform the per-event analysis
 		void analyze(const Event& event) {
 
-			const UnstableParticles np = apply<UnstableParticles>(event, "np");
+			const UnstableParticles up = apply<UnstableParticles>(event, "up");
 			const double c = cent();
 			const CentralityProjection& cent = apply<CentralityProjection>(event, "CMULT");
-			const Particles unstableParticles = np.particles();
+			const Particles unstableParticles = up.particles();
 
-			
+			if ((c < 0.) || (c > 92.)) vetoEvent;
+
+			if ((c >= 0.) && (c < 5.)) {
+	
+				
 		}
 
 
