9225600897b220c47d7f0a788097d6283b9f5bef
cnattras <saccharomyces.cerevisae@gmail.com>
2021-08-06 16:21:12 -0400

    fixing RunAnalysis

--- PHENIX_2020_I1798493/RunAnalysis.sh
+++ PHENIX_2020_I1798493/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2020_I1798493.so PHENIX_2020_I1798493.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2020_I1798493:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2020_I1798493:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
