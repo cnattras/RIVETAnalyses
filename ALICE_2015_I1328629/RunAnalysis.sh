d9b018959d40105581aa9b1fc650db95b5cc45ed
patelria007 <patelria07@gmail.com>
2021-08-27 16:15:19 -0400

    Updated RunAnalysis script to not use centrality

--- ALICE_2015_I1328629/RunAnalysis.sh
+++ ALICE_2015_I1328629/RunAnalysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rivet-build RivetALICE_2015_I1328629.so ALICE_2015_I1328629.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a ALICE_2015_I1328629 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -a ALICE_2015_I1328629 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
 
 ### ALICE_2015_I1328629:cent=GEN:beam=dAU200
