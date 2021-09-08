c16b7fe6650e4166f4873441d5926e66cc0089a6
patelria007 <patelria07@gmail.com>
2021-09-08 13:41:56 -0400

    Removed 3 unnecessary include files

--- ALICE_2015_I1328629/ALICE_2015_I1328629.cc
+++ ALICE_2015_I1328629/ALICE_2015_I1328629.cc
@@ -2,9 +2,6 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
-#include "Rivet/Projections/DressedLeptons.hh"
-#include "Rivet/Projections/MissingMomentum.hh"
-#include "Rivet/Projections/PromptFinalState.hh"
 #include "Rivet/Projections/AliceCommon.hh"
 #include "Rivet/Tools/AliceCommon.hh"
 #include <stdio.h>
