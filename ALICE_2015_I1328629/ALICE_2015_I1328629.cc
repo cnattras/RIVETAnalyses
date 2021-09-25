6b32fb77433cf69d5722169d67a3906d0f031379
patelria007 <patelria07@gmail.com>
2021-09-24 21:13:18 -0400

    Added correct histograms and improved names. Corrected cuts.

--- ALICE_2015_I1328629/ALICE_2015_I1328629.cc
+++ ALICE_2015_I1328629/ALICE_2015_I1328629.cc
@@ -5,8 +5,6 @@
 #include "Rivet/Projections/AliceCommon.hh"
 #include "Rivet/Tools/AliceCommon.hh"
 #include <stdio.h>
-#include <vector>
-#include <string>
 
 namespace Rivet {
 
@@ -20,7 +18,6 @@ namespace Rivet {
 
 			/// @name Analysis methods
 			///@{
-			vector <string> histogramNames;
 
 			/// Book histograms and initialise projections before the run
 			void init() {
@@ -28,7 +25,7 @@ namespace Rivet {
 				// Initialise and register projections
 
 				// The basic final-state projection: all final-state particles within the given eta acceptance
-				const FinalState fs(Cuts::abseta < 1.5);
+				const FinalState fs(Cuts::abseta < 0.9);
 
 				/* The final-state particles declared above are clustered using FastJet with the anti-kT 
 				   algorithm and a jet-radius parameter 0.4 muons & neutrinos are excluded from the clustering */
@@ -59,14 +56,20 @@ namespace Rivet {
 
 				book(_c["sow"], "sow");
 
-				// Creates histograms for all Tables in .yoda file (73 tables in total, all in order)
-				char name[100];
-				for (int i = 1; i < 74; i++) {
-					snprintf(name, sizeof(name), "Table%d", i);
-					histogramNames.push_back(name)  // Keeps track of all table names
-					book(_h[name], i, 1, 1);
-				}
-
+				// Creates histograms 
+				book(_h["all3JetFinders_R04"], 1, 1, 1);  // Figure 2
+				book(_h["antiKTR02_03_04_06"], 4, 1, 1);  // Figure 3
+				book(_h["antiKT_R04_06_ALICE_ATLAS"], 8, 1, 1); // Figure 4
+				book(_h["antiKT_ALICEvsMCAlgs_R02_04_06_Eta07_05_03"], 10, 1, 1);  // Figure 5
+				book(_h["ratio_AntiKT_R0204_0206_Eta03"], 13, 1, 1);  // Figure 6
+				book(_h["mean_ALICEvsMC_R06_04_02_Eta_03_05_07"], 15, 1, 1);  // Figure 7
+				book(_h["pTR02_Eta07Bins_2030_3040_4060_6080"], 18, 1, 1); // Figure 8
+				book(_h["pTR04_Eta05Bins_2030_3040_4060_6080"], 22, 1, 1);  // Figure 9
+				book(_h["pTR04_Eta03Bins_2030_3040_4060_6080"], 26, 1, 1);  // Figure 10
+				book(_h["avgR80pT_R02_04_06_Eta07_05_03"], 30, 1, 1);  // Figure 11
+				book(_h["pTSpectraR04_Eta05Bins_ALICEvsMC"], 33, 1, 1);  // Figure 12
+				book(_h["pTSpectraR04_Eta05Bins0to1"], 37, 1, 1);  // Figure 13
+				book(_h["pTSpectraR04_Eta05Bins0to6", 41, 1, 1]);  // Figure 14
 			}
 
 
@@ -79,18 +82,24 @@ namespace Rivet {
 				const ALICE::PrimaryParticles aprim = apply<ALICE::PrimaryParticles>(event, "aprim");
 				const Particles ALICEparticles = aprim.particles();
 
-				Jets jetsAKT04 = apply<FastJets>(event, "jetsAKT04").jetsByPt(Cuts::pT >= 0*GeV && Cuts::abseta <0.5);
-				Jets jetsKT04 = apply<FastJets>(event, "jetsKT04").jetsByPt(Cuts::pT >= 0*GeV && Cuts::abseta <0.5);
-				Jets jetsCONE04 = apply<FastJets>(event, "jetsCONE04").jetsByPt(Cuts::pT >= 0*GeV && Cuts::abseta <0.5);
+				Jets jetsAKT02 = apply<FastJets>(event, "jetsAKT02").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsKT02 = apply<FastJets>(event, "jetsKT02").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsCONE02 = apply<FastJets>(event, "jetsCONE02").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+
+				Jets jetsAKT04 = apply<FastJets>(event, "jetsAKT04").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsKT04 = apply<FastJets>(event, "jetsKT04").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsCONE04 = apply<FastJets>(event, "jetsCONE04").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+
+				Jets jetsAKT06 = apply<FastJets>(event, "jetsAKT06").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsKT06 = apply<FastJets>(event, "jetsKT06").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
+				Jets jetsCONE06 = apply<FastJets>(event, "jetsCONE06").jetsByPt(Cuts::pT > 0.15*GeV && Cuts::abseta < 0.9);
 				
 				// FJjets.calc(ALICEparticles); //give ALICE primary particles to FastJet projection
 
 				// "sow" - Sum of Weights - Adding up equivalent proton-proton collisions that occurred
-				// _c["sow"]->fill();
-				// for(auto jet : jetsAKT04)
-				// {
-				// 	_h["Figure1"]->fill(jet.pT()/GeV);
-				// }
+				_c["sow"]->fill();
+				for(auto jet : jetsAKT04) { _h["all3JetFinders_R04"]->fill(jet.pT()/GeV); }
+
 			}
 
 
