4151f6e1f2e60f97752c038b11501880f55c21bc
patelria007 <patelria07@gmail.com>
2021-11-03 13:55:30 -0400

    Updated plots and Figure 12 histogram

--- ALICE_2015_I1328629/ALICE_2015_I1328629.cc
+++ ALICE_2015_I1328629/ALICE_2015_I1328629.cc
@@ -8,7 +8,6 @@
 
 namespace Rivet {
 
-
 	/// @brief Add a short analysis description here
 	class ALICE_2015_I1328629 : public Analysis {
 		public:
@@ -18,6 +17,22 @@ namespace Rivet {
 
 			/// @name Analysis methods
 			///@{
+
+			void FillRadialDistribution(Profile1DPtr prof1D, Jet jet)
+			{
+				std::vector<double> sum(prof1D->numBins(), 0.);
+				for(auto p : jet.particles())
+				{
+					double r = deltaR(p.eta(), p.phi(), jet.eta(), jet.phi());
+					int bin = prof1D->binIndexAt(r);
+					if(bin >= 0) sum[bin] += p.pT()/GeV;
+				}
+				for(unsigned int i = 0; i < sum.size(); i++) {
+					prof1D->fill(prof1D->bin(i).xMid(),sum[i]/prof1D->bin(i).xWidth());
+				}
+			}
+
+
 
 			/// Book histograms and initialise projections before the run
 			void init() {
@@ -53,6 +68,10 @@ namespace Rivet {
 
 				// Create counter
 				book(_c["sow"], "sow");
+				book(_c["sow2030"], "sow2030");
+				book(_c["sow3040"], "sow3040");
+				book(_c["sow4060"], "sow4060");
+				book(_c["sow6080"], "sow6080");
 
 				// Creates histograms 
 				book(_h["CrossSectionAntikT_R04"], 1, 1, 1);  // Figure 2 Anti-kT
@@ -85,24 +104,24 @@ namespace Rivet {
 				book(_p["mean_ALICEvsMC_R04_Eta_05"], 16, 1, 1);  // Figure 7
 				book(_p["mean_ALICEvsMC_R06_Eta_03"], 17, 1, 1);  // Figure 7
 
-				book(_h["pTR02_Eta07_2030"], 18, 1, 1);  // Figure 8
-				book(_h["pTR02_Eta07_3040"], 19, 1, 1);  // Figure 8
-				book(_h["pTR02_Eta07_4060"], 20, 1, 1);  // Figure 8
-				book(_h["pTR02_Eta07_6080"], 21, 1, 1);  // Figure 8
+				book(_p["pTR02_Eta07_2030"], 18, 1, 1);  // Figure 8
+				book(_p["pTR02_Eta07_3040"], 19, 1, 1);  // Figure 8
+				book(_p["pTR02_Eta07_4060"], 20, 1, 1);  // Figure 8
+				book(_p["pTR02_Eta07_6080"], 21, 1, 1);  // Figure 8
 
-				book(_h["pTR04_Eta05_2030"], 22, 1, 1);  // Figure 9
-				book(_h["pTR04_Eta05_3040"], 23, 1, 1);  // Figure 9
-				book(_h["pTR04_Eta05_4060"], 24, 1, 1);  // Figure 9
-				book(_h["pTR04_Eta05_6080"], 25, 1, 1);  // Figure 9
+				book(_p["pTR04_Eta05_2030"], 22, 1, 1);  // Figure 9
+				book(_p["pTR04_Eta05_3040"], 23, 1, 1);  // Figure 9
+				book(_p["pTR04_Eta05_4060"], 24, 1, 1);  // Figure 9
+				book(_p["pTR04_Eta05_6080"], 25, 1, 1);  // Figure 9
 
-				book(_h["pTR06_Eta03_2030"], 26, 1, 1);  // Figure 10
-				book(_h["pTR06_Eta03_3040"], 27, 1, 1);  // Figure 10
-				book(_h["pTR06_Eta03_4060"], 28, 1, 1);  // Figure 10
-				book(_h["pTR06_Eta03_6080"], 29, 1, 1);  // Figure 10
+				book(_p["pTR06_Eta03_2030"], 26, 1, 1);  // Figure 10
+				book(_p["pTR06_Eta03_3040"], 27, 1, 1);  // Figure 10
+				book(_p["pTR06_Eta03_4060"], 28, 1, 1);  // Figure 10
+				book(_p["pTR06_Eta03_6080"], 29, 1, 1);  // Figure 10
 
-				book(_h["avgpT_R02_Eta07"], 30, 1, 1);  // Figure 11
-				book(_h["avgpT_R04_Eta05"], 31, 1, 1);  // Figure 11
-				book(_h["avgpT_R06_Eta03"], 32, 1, 1);  // Figure 11
+				book(_p["avgpT_R02_Eta07"], 30, 1, 1);  // Figure 11
+				book(_p["avgpT_R04_Eta05"], 31, 1, 1);  // Figure 11
+				book(_p["avgpT_R06_Eta03"], 32, 1, 1);  // Figure 11
 
 				book(_h["pTSpectraR04_Eta05_2030_ALICEvsMC"], 33, 1, 1);  // Figure 12
 				book(_h["pTSpectraR04_Eta05_3040_ALICEvsMC"], 34, 1, 1);  // Figure 12
@@ -126,6 +145,7 @@ namespace Rivet {
 			void analyze(const Event& event) {
 				//Increment the counter to keep track of the cross section
 				_c["sow"]->fill();
+				
 
 				// Retrieve clustered jets, sorted by pT, with a minimum pT cut
 				const ALICE::PrimaryParticles aprim = apply<ALICE::PrimaryParticles>(event, "aprim");
@@ -136,19 +156,27 @@ namespace Rivet {
 				jetsAKTR02FJ.calc(ALICEparticles); //give ALICE primary particles to FastJet projection
 				Jets jetsAKTR02 = jetsAKTR02FJ.jetsByPt(Cuts::pT >= 20*GeV && Cuts::abseta < 0.7);
 
-				if(jetsAKTR02.size() != 0)
-					_p["mean_ALICEvsMC_R02_Eta_07"]->fill(jetsAKTR02[0].pT()/GeV, jetsAKTR02[0].particles().size());
+				if(jetsAKTR02.size() != 0) {
+					_p["mean_ALICEvsMC_R02_Eta_07"]->fill(jetsAKTR02[0].pT()/GeV, jetsAKTR02[0].particles().size()); // Figure 7
+					if(jetsAKTR02[0].pT() >= 20*GeV && jetsAKTR02[0].pT() < 30*GeV) {
+						FillRadialDistribution(_p["pTR02_Eta07_2030"], jetsAKTR02[0]); // Figure 8
+					}
+					if(jetsAKTR02[0].pT() >= 30*GeV && jetsAKTR02[0].pT() < 40*GeV) {
+						FillRadialDistribution(_p["pTR02_Eta07_3040"], jetsAKTR02[0]); // Figure 8
+					}
+					if(jetsAKTR02[0].pT() >= 40*GeV && jetsAKTR02[0].pT() < 60*GeV) {
+						FillRadialDistribution(_p["pTR02_Eta07_4060"], jetsAKTR02[0]); // Figure 8
+					}
+					if(jetsAKTR02[0].pT() >= 60*GeV && jetsAKTR02[0].pT() < 80*GeV) {
+						FillRadialDistribution(_p["pTR02_Eta07_6080"], jetsAKTR02[0]); // Figure 8
+					}
+				}
 
 				for(auto jet: jetsAKTR02) {
 					_h["antiKTR02"]->fill(jet.pT()/GeV); // Figure 3
 					_h["ALICEvsMC_R02_Eta07"]->fill(jet.pT()/GeV); // Figure 5
 					_h["Ratio02_Numerator"]->fill(jet.pT()/GeV); // Figure 6
-					// _h["mean_ALICEvsMC_R02_Eta_07"]->fill(jet.pT()/GeV); // Figure 7
-					_h["pTR02_Eta07_2030"]->fill(jet.pT()/GeV); // Figure 8
-					_h["pTR02_Eta07_3040"]->fill(jet.pT()/GeV); // Figure 8
-					_h["pTR02_Eta07_4060"]->fill(jet.pT()/GeV); // Figure 8
-					_h["pTR02_Eta07_6080"]->fill(jet.pT()/GeV); // Figure 8
-					_h["avgpT_R02_Eta07"]->fill(jet.pT()/GeV); // Figure 11
+					// _h["avgpT_R02_Eta07"]->fill(jet.pT()/GeV); // Figure 11
 				}
 
 				// Anti-KT alg. - Resolution = 0.3, Eta = 0.6 (From 0.9 - 0.3)
@@ -165,8 +193,37 @@ namespace Rivet {
 				jetsAKTR04FJ.calc(ALICEparticles); 
 				Jets jetsAKTR04 = jetsAKTR04FJ.jetsByPt(Cuts::pT >= 20.*GeV && Cuts::abseta < 0.5); 
 
-				if(jetsAKTR04.size() != 0)
-					_p["mean_ALICEvsMC_R04_Eta_05"]->fill(jetsAKTR04[0].pT()/GeV, jetsAKTR04[0].particles().size());
+				if(jetsAKTR04.size() != 0) {
+					_p["mean_ALICEvsMC_R04_Eta_05"]->fill(jetsAKTR04[0].pT()/GeV, jetsAKTR04[0].particles().size()); // Figure 7
+					if(jetsAKTR04[0].pT() >= 20*GeV && jetsAKTR04[0].pT() < 30*GeV) {
+						_c["sow2030"]->fill();
+						FillRadialDistribution(_p["pTR04_Eta05_2030"], jetsAKTR04[0]); // Figure 9
+						for (auto p : jetsAKTR04[0].particles()) {
+							_h["pTSpectraR04_Eta05_2030_ALICEvsMC"]->fill(p.pT()/GeV); // Figure 12
+						}
+					}
+					if(jetsAKTR04[0].pT() >= 30*GeV && jetsAKTR04[0].pT() < 40*GeV) {
+						_c["sow3040"]->fill();
+						FillRadialDistribution(_p["pTR04_Eta05_3040"], jetsAKTR04[0]); // Figure 9
+						for (auto p : jetsAKTR04[0].particles()) {
+							_h["pTSpectraR04_Eta05_3040_ALICEvsMC"]->fill(p.pT()/GeV); // Figure 12
+						}
+					}
+					if(jetsAKTR04[0].pT() >= 40*GeV && jetsAKTR04[0].pT() < 60*GeV) {
+						_c["sow4060"]->fill();
+						FillRadialDistribution(_p["pTR04_Eta05_4060"], jetsAKTR04[0]); // Figure 9
+						for (auto p : jetsAKTR04[0].particles()) {
+							_h["pTSpectraR04_Eta05_4060_ALICEvsMC"]->fill(p.pT()/GeV); // Figure 12
+						}
+					}
+					if(jetsAKTR04[0].pT() >= 60*GeV && jetsAKTR04[0].pT() < 80*GeV) {
+						_c["sow6080"]->fill();
+						FillRadialDistribution(_p["pTR04_Eta05_6080"], jetsAKTR04[0]); // Figure 9
+						for (auto p : jetsAKTR04[0].particles()) {
+							_h["pTSpectraR04_Eta05_6080_ALICEvsMC"]->fill(p.pT()/GeV); // Figure 12
+						}
+					}
+				}
 
 				for(auto jet : jetsAKTR04) {
 					_h["CrossSectionAntikT_R04"]->fill(jet.pT()/GeV); // Figure 2
@@ -174,16 +231,7 @@ namespace Rivet {
 					_h["antiKT_R04_ALICE_ATLAS"]->fill(jet.pT()/GeV); // Figure 4
 					_h["ALICEvsMC_R04_Eta05"]->fill(jet.pT()/GeV); // Figure 5
 					_h["Ratio04_Denominator"]->fill(jet.pT()/GeV); // Figure 6
-					// _h["mean_ALICEvsMC_R04_Eta_05"]->fill(jet.pT()/GeV); // Figure 7
-					_h["pTR04_Eta05_2030"]->fill(jet.pT()/GeV); // Figure 9
-					_h["pTR04_Eta05_3040"]->fill(jet.pT()/GeV); // Figure 9
-					_h["pTR04_Eta05_4060"]->fill(jet.pT()/GeV); // Figure 9
-					_h["pTR04_Eta05_6080"]->fill(jet.pT()/GeV); // Figure 9
-					_h["avgpT_R04_Eta05"]->fill(jet.pT()/GeV); // Figure 11
-					_h["pTSpectraR04_Eta05_2030_ALICEvsMC"]->fill(jet.pT()/GeV); // Figure 12
-					_h["pTSpectraR04_Eta05_3040_ALICEvsMC"]->fill(jet.pT()/GeV); // Figure 12
-					_h["pTSpectraR04_Eta05_4060_ALICEvsMC"]->fill(jet.pT()/GeV); // Figure 12
-					_h["pTSpectraR04_Eta05_6080_ALICEvsMC"]->fill(jet.pT()/GeV); // Figure 12
+					// _h["avgpT_R04_Eta05"]->fill(jet.pT()/GeV); // Figure 11
 					_h["pTSpectraR04_Eta05_2030_0to1"]->fill(jet.pT()/GeV); // Figure 13
 					_h["pTSpectraR04_Eta05_3040_0to1"]->fill(jet.pT()/GeV); // Figure 13
 					_h["pTSpectraR04_Eta05_4060_0to1"]->fill(jet.pT()/GeV); // Figure 13
@@ -192,7 +240,6 @@ namespace Rivet {
 					_h["pTSpectraR04_Eta05_3040_0to6"]->fill(jet.pT()/GeV); // Figure 14
 					_h["pTSpectraR04_Eta05_4060_0to6"]->fill(jet.pT()/GeV); // Figure 14
 					_h["pTSpectraR04_Eta05_6080_0to6"]->fill(jet.pT()/GeV); // Figure 14
-
 				}
 
 				// KT alg. - Resolution = 0.4, Eta = 0.5 (From 0.9 - 0.4)
@@ -216,20 +263,28 @@ namespace Rivet {
 				jetsAKTR06FJ.calc(ALICEparticles);
 				Jets jetsAKTR06 = jetsAKTR06FJ.jetsByPt(Cuts::pT >= 20*GeV && Cuts::abseta < 0.3);
 				
-				if(jetsAKTR06.size() != 0)
-					_p["mean_ALICEvsMC_R06_Eta_03"]->fill(jetsAKTR06[0].pT()/GeV, jetsAKTR06[0].particles().size());
+				if(jetsAKTR06.size() != 0) {
+					_p["mean_ALICEvsMC_R06_Eta_03"]->fill(jetsAKTR06[0].pT()/GeV, jetsAKTR06[0].particles().size()); // Figure 7
+					if(jetsAKTR06[0].pT() >= 20*GeV && jetsAKTR06[0].pT() < 30*GeV) {
+						FillRadialDistribution(_p["pTR06_Eta03_2030"], jetsAKTR06[0]); // Figure 10
+					}
+					if(jetsAKTR06[0].pT() >= 30*GeV && jetsAKTR06[0].pT() < 40*GeV) {
+						FillRadialDistribution(_p["pTR06_Eta03_3040"], jetsAKTR06[0]); // Figure 10
+					}
+					if(jetsAKTR06[0].pT() >= 40*GeV && jetsAKTR06[0].pT() < 60*GeV) {
+						FillRadialDistribution(_p["pTR06_Eta03_4060"], jetsAKTR06[0]); // Figure 10
+					}
+					if(jetsAKTR06[0].pT() >= 60*GeV && jetsAKTR06[0].pT() < 80*GeV) {
+						FillRadialDistribution(_p["pTR06_Eta03_6080"], jetsAKTR06[0]); // Figure 10
+					}
+				}
 
 				for(auto jet: jetsAKTR06) {
 					_h["antiKTR06"]->fill(jet.pT()/GeV); // Figure 3
 					_h["antiKT_R06_ALICE_ATLAS"]->fill(jet.pT()/GeV); // Figure 4
 					_h["ALICEvsMC_R06_Eta03"]->fill(jet.pT()/GeV); // Figure 5
 					_h["Ratio06_Denominator"]->fill(jet.pT()/GeV); // Figure 6
-					// _h["mean_ALICEvsMC_R06_Eta_03"]->fill(jet.pT()/GeV); // Figure 7
-					_h["pTR06_Eta03_2030"]->fill(jet.pT()/GeV); // Figure 10
-					_h["pTR06_Eta03_3040"]->fill(jet.pT()/GeV); // Figure 10
-					_h["pTR06_Eta03_4060"]->fill(jet.pT()/GeV); // Figure 10
-					_h["pTR06_Eta03_6080"]->fill(jet.pT()/GeV); // Figure 10
-					_h["avgpT_R06_Eta03"]->fill(jet.pT()/GeV); // Figure 11
+					// _h["avgpT_R06_Eta03"]->fill(jet.pT()/GeV); // Figure 11
 				}
 			}
 
@@ -263,36 +318,18 @@ namespace Rivet {
 				divide(_h["Ratio02_Numerator"], _h["Ratio04_Denominator"], _s["Ratio0204"]);
 				divide(_h["Ratio02_Numerator"], _h["Ratio06_Denominator"], _s["Ratio0206"]);
 
-				// Figure 7 does not need to be normalized
-
-				// Figure 8
-				_h["pTR02_Eta07_2030"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
-				_h["pTR02_Eta07_3040"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
-				_h["pTR02_Eta07_4060"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
-				_h["pTR02_Eta07_6080"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
-
-				// Figure 9
-				_h["pTR04_Eta05_2030"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTR04_Eta05_3040"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTR04_Eta05_4060"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTR04_Eta05_6080"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-
-				// Figure 10
-				_h["pTR06_Eta03_2030"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
-				_h["pTR06_Eta03_3040"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
-				_h["pTR06_Eta03_4060"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
-				_h["pTR06_Eta03_6080"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
+				// Figure 7, 8, 9, & 10 do not need to be normalized
 
 				// Figure 11 
-				_h["avgpT_R02_Eta07"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
-				_h["avgpT_R04_Eta05"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["avgpT_R06_Eta03"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
+				// _h["avgpT_R02_Eta07"]->scaleW(crossSection()/(millibarn*1.4*_c["sow"]->sumW()));
+				// _h["avgpT_R04_Eta05"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
+				// _h["avgpT_R06_Eta03"]->scaleW(crossSection()/(millibarn*0.6*_c["sow"]->sumW()));
 
 				// Figure 12
-				_h["pTSpectraR04_Eta05_2030_ALICEvsMC"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTSpectraR04_Eta05_3040_ALICEvsMC"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTSpectraR04_Eta05_4060_ALICEvsMC"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
-				_h["pTSpectraR04_Eta05_6080_ALICEvsMC"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
+				_h["pTSpectraR04_Eta05_2030_ALICEvsMC"]->scaleW(1/_c["sow2030"]->sumW());
+				_h["pTSpectraR04_Eta05_3040_ALICEvsMC"]->scaleW(1/_c["sow3040"]->sumW());
+				_h["pTSpectraR04_Eta05_4060_ALICEvsMC"]->scaleW(1/_c["sow4060"]->sumW());
+				_h["pTSpectraR04_Eta05_6080_ALICEvsMC"]->scaleW(1/_c["sow6080"]->sumW());
 
 				// Figure 13
 				_h["pTSpectraR04_Eta05_2030_0to1"]->scaleW(crossSection()/(millibarn*_c["sow"]->sumW()));
--- ALICE_2015_I1328629/ALICE_2015_I1328629.plot
+++ ALICE_2015_I1328629/ALICE_2015_I1328629.plot
@@ -82,12 +82,149 @@ END PLOT
 
 BEGIN PLOT /ALICE_2015_I1328629/d13-x01-y01
 Title=Ratios of Jet Cross Sections Anti-KT R = $\frac{0.2}{0.4}$
-YLabel=$\frac{d^2\sigma^{\text{jet, ch}}}{d\emph{p}_{T}d\eta}$$\text{(mb \emph{c}/GeV)}$
+YLabel=$\frac{\sigma^{\text{jet, ch}} \text{R = 0.2}}{\sigma^{\text{jet, ch}} \text{(R = 0.4)}}$
+YLabelSep=5
 XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+XLabelSep=5
 END PLOT
 
 BEGIN PLOT /ALICE_2015_I1328629/d14-x01-y01
 Title=Ratios of Jet Cross Sections Anti-KT R = $\frac{0.2}{0.6}$
-YLabel=$\frac{d^2\sigma^{\text{jet, ch}}}{d\emph{p}_{T}d\eta}$$\text{(mb \emph{c}/GeV)}$
+YLabel=$\frac{\sigma^{\text{jet, ch}} \text{R = 0.2}}{\sigma^{\text{jet, ch}} \text{(R = 0.6)}}$
 XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
 END PLOT
+
+# Figure 7
+
+BEGIN PLOT /ALICE_2015_I1328629/d15-x01-y01
+Title=Mean charged particle multiplicity in the leading charged jet (R = 0.2)
+YLabel=$\langle N_{\text{ch}} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d16-x01-y01
+Title=Mean charged particle multiplicity in the leading charged jet (R = 0.4)
+YLabel=$\langle N_{\text{ch}} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d17-x01-y01
+Title=Mean charged particle multiplicity in the leading charged jet (R = 0.6)
+YLabel=$\langle N_{\text{ch}} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+END PLOT
+
+# Figure 8
+
+BEGIN PLOT /ALICE_2015_I1328629/d18-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 20 - 30 (R = 0.2)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d19-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 30 - 40 (R = 0.2)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d20-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 40 - 60 (R = 0.2)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d21-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 60 - 80 (R = 0.2)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+# Figure 9
+
+BEGIN PLOT /ALICE_2015_I1328629/d22-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 20 - 30 (R = 0.4)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d23-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 30 - 40 (R = 0.4)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d24-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 40 - 60 (R = 0.4)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d25-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 60 - 80 (R = 0.4)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+
+# Figure 10
+
+BEGIN PLOT /ALICE_2015_I1328629/d26-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 20 - 30 (R = 0.6)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d27-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 30 - 40 (R = 0.6)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d28-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 40 - 60 (R = 0.6)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+END PLOT
+
+BEGIN PLOT /ALICE_2015_I1328629/d29-x01-y01
+Title=Radial Distributions of $p_{T}$, Bin 60 - 80 (R = 0.6)
+YLabel=$\langle \text{d}p^{\text{sum}}_{\text{T}}/\text{d}r \rangle (\text{GeV}/\emph{c})$
+XLabel=Distance $\emph{r}$
+
+# Figure 11
+
+BEGIN PLOT /ALICE_2015_I1328629/d30-x01-y01
+Title=Distributions of Average Radius '$\langle R_{80} \rangle$' (R = 0.2)
+YLabel=$\langle R_{80} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+
+BEGIN PLOT /ALICE_2015_I1328629/d31-x01-y01
+Title=Distributions of Average Radius '$\langle R_{80} \rangle$' (R = 0.4)
+YLabel=$\langle R_{80} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+
+BEGIN PLOT /ALICE_2015_I1328629/d32-x01-y01
+Title=Distributions of Average Radius '$\langle R_{80} \rangle$' (R = 0.6)
+YLabel=$\langle R_{80} \rangle$
+XLabel=$p_{T}^{\text{jet, ch}}$ $\text{(GeV/\emph{c})}$
+
+# Figure 12
+
+BEGIN PLOT /ALICE_2015_I1328629/d33-x01-y01
+Title=Charged Particle $p_{T}$ Spectra $\text{d}N/\text{d}p_{T}$ in Leading Jets, Bin 20 - 30
+YLabel=$1/N_{\text{jets}}\text{dN/d}p_{T} (c/\text{GeV})$
+XLabel=$p_{T}^{\text{particle}}$ $\text{(GeV/\emph{c})}$
+
+BEGIN PLOT /ALICE_2015_I1328629/d34-x01-y01
+Title=Charged Particle $p_{T}$ Spectra $\text{d}N/\text{d}p_{T}$ in Leading Jets, Bin 30 - 40
+YLabel=$1/N_{\text{jets}}\text{dN/d}p_{T} (c/\text{GeV})$
+XLabel=$p_{T}^{\text{particle}}$ $\text{(GeV/\emph{c})}$
+
+BEGIN PLOT /ALICE_2015_I1328629/d35-x01-y01
+Title=Charged Particle $p_{T}$ Spectra $\text{d}N/\text{d}p_{T}$ in Leading Jets, Bin 40 - 60
+YLabel=$1/N_{\text{jets}}\text{dN/d}p_{T} (c/\text{GeV})$
+XLabel=$p_{T}^{\text{particle}}$ $\text{(GeV/\emph{c})}$
+
+BEGIN PLOT /ALICE_2015_I1328629/d36-x01-y01
+Title=Charged Particle $p_{T}$ Spectra $\text{d}N/\text{d}p_{T}$ in Leading Jets, Bin 60 - 80
+YLabel=$1/N_{\text{jets}}\text{dN/d}p_{T} (c/\text{GeV})$
+XLabel=$p_{T}^{\text{particle}}$ $\text{(GeV/\emph{c})}$
