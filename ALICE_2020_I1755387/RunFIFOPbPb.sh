e56a25bb6f9576c4ef7e009ca3a3559fe466dee0
Christine Nattrass <cnattras@acf-login8.acf.nics.utk.edu>
2021-11-19 13:50:37 -0500

    adding runfifo for pb

---
+++ ALICE_2020_I1755387/RunFIFOPbPb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#ATTENTION! FIFO requires a lot of virtual memory. When entering in debug more, use: qsub -I -l nodes=1:ppn=4 -q debug
+#This will request 4 cores and sufficient virtual memory
+#Copy the runPythia file to the same directory where you have your Rivet Analysis
+#On ACF you can find runPythia here: /lustre/haven/proj/UTK0019/Rivet
+#Change the name "PHENIX_2008_I778168" to your Rivet Analysis
+#Have your centrality calibration file in the same folder or point $CALIBRATION to the right path/file
+date
+ANALYSIS="ALICE_2020_I1755387"
+ANALYSIS_DIR=$PWD
+source /lustre/haven/proj/UTK0019/Rivet/rivet.sh
+rm RivetALICE_2020_I1755387.so
+rivet-build RivetALICE_2020_I1755387.so ALICE_2020_I1755387.cc
+FIFOFILE="fifoNATTRASS$ANALYSIS.hepmc"
+#Number of events
+NEVENTS="2000"
+#Beams
+BEAM1="Pb"
+BEAM2="Pb"
+#Energy in the center of mass system
+CMS_ENERGY="5020"
+#Pythia seed. If 0 pythia will generate a random number
+GENERATOR_SEED="0"
+#Min and max pT-hard. if PTHARDMIN > PTHARDMAX then pythia switches to minimum bias (No pt-hard)
+PTHARDMIN="0"
+PTHARDMAX="-1"
+#Centrality Calibration file
+CALIBRATION="../Centralities/Calibration/calibration_ALICE_PbPb5020GeV.yoda"
+#Flags of your analysis (Ex. centrality: cent=GEN)
+RIVET_FLAGS=""
+rm /tmp/$FIFOFILE
+mkfifo /tmp/$FIFOFILE
+#rivet --pwd -p ../Centralities/Calibration/calibration_ALICE_PbPb5020GeV.yoda -a ALICE_2020_I1755387:cent=GEN -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+./runPythia /tmp $FIFOFILE $NEVENTS $BEAM1 $BEAM2 $CMS_ENERGY $GENERATOR_SEED $PTHARDMIN $PTHARDMAX & rivet --pwd -p $CALIBRATION -a $ANALYSIS$RIVET_FLAGS -o Rivet.yoda /tmp/$FIFOFILE
+rm /tmp/$FIFOFILE
+date
