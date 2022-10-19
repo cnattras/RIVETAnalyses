f636a570a9009df471feef0a15944ad8539d8d7a
Sean Grace <sgrace3@acf-login5.acf.nics.utk.edu>
2022-10-19 14:04:29 -0400

    adding missing histos

--- PHENIX_2001_I562409/PHENIX_2001_I562409.cc
+++ PHENIX_2001_I562409/PHENIX_2001_I562409.cc
@@ -5,7 +5,7 @@
 #include "Rivet/Projections/DressedLeptons.hh"
 #include "Rivet/Projections/MissingMomentum.hh"
 #include "Rivet/Projections/PromptFinalState.hh"
-#include "../Centralities/RHICCentrality.hh"
+#include "RHICCentrality.hh"
 #include "Rivet/Projections/UnstableParticles.hh"
 #include "Rivet/Projections/HadronicFinalState.hh"
 
@@ -43,10 +43,45 @@ namespace Rivet {
       book(_h["ChHadronsCent60_80"], 6, 1, 1);
       book(_c["Cent0_10"], "Cent0_10");
       book(_c["Cent60_80"], "Cent60_80");
+      book(_c["pp_130"], "Sow_pp");
       book(_h["Pi0PbScCent0_10"], 1, 1, 1);
       book(_h["Pi0PbScCent60_80"], 2, 1, 1);
       book(_h["Pi0PbGlCent0_10"], 3, 1, 1);
       book(_h["Pi0PbGlCent60_80"], 4, 1, 1);
+      string refnameRaa1  = mkAxisCode(7,1,1);
+      const Scatter2D& refdataRaa1 =refData(refnameRaa1);
+      book(_h["c10Pt_AuAu130_t7"], refnameRaa1 + "_AuAu130", refdataRaa1);
+      book(_h["c10Pt130_pp_t7"], refnameRaa1 + "_pp130", refdataRaa1);
+      book(_s["Raa_c010_AuAu130_t7"], refnameRaa1);
+      string refnameRaa2  = mkAxisCode(8,1,1);
+      const Scatter2D& refdataRaa2 =refData(refnameRaa2);
+      book(_h["c10Pt_AuAu130_t8"], refnameRaa2 + "_AuAu130", refdataRaa2);
+      book(_h["c10Pt130_pp_t8"], refnameRaa2 + "_pp130", refdataRaa2);
+      book(_s["Raa_c010_AuAu130_t8"], refnameRaa2);
+      string refnameRaa3  = mkAxisCode(9,1,1);
+      const Scatter2D& refdataRaa3 =refData(refnameRaa3);
+      book(_h["c10Pt_AuAu130_t9"], refnameRaa3 + "_AuAu130", refdataRaa3);
+      book(_h["c10Pt130_pp_t9"], refnameRaa3 + "_pp130", refdataRaa3);
+      book(_s["Raa_c010_AuAu130_t9"], refnameRaa3);
+      
+      string refnameRCP1  = mkAxisCode(10,1,1);
+      const Scatter2D& refdataRCP1 =refData(refnameRCP1);
+      book(_h["c10Pt_AuAu130_t10"], refnameRCP1 + "_cAuAu130", refdataRCP1);
+      book(_h["p80Pt130_AuAu130_t10"], refnameRCP1 + "_pAuAu130", refdataRCP1);
+      book(_s["RCP_AuAu130_t10"], refnameRCP1);
+
+      string refnameRCP2  = mkAxisCode(11,1,1);
+      const Scatter2D& refdataRCP2 =refData(refnameRCP2);
+      book(_h["c10Pt_AuAu130_t11"], refnameRCP2 + "_cAuAu130", refdataRCP2);
+      book(_h["p80Pt130_AuAu130_t11"], refnameRCP2 + "_pAuAu130", refdataRCP2);
+      book(_s["RCP_AuAu130_t11"], refnameRCP2);
+
+      string refnameRCP3  = mkAxisCode(12,1,1);
+      const Scatter2D& refdataRCP3 =refData(refnameRCP3);
+      book(_h["c10Pt_AuAu130_t12"], refnameRCP3 + "_cAuAu130", refdataRCP3);
+      book(_h["p80Pt130_AuAu130_t12"], refnameRCP3 + "_pAuAu130", refdataRCP3);
+      book(_s["RCP_AuAu130_t12"], refnameRCP3);
+
 
     }
 
@@ -64,19 +99,52 @@ namespace Rivet {
       const Particles pi0UPParticles = pi0UP.particles();
 
       double inv2PI = 1./(2.*M_PI);
+      const ParticlePair& beam = beams();
+      string collSys;
 
-            if(cent < 10.) //Check centrality of the event
+      if (beam.first.pid() == 1000791970 && beam.second.pid() == 1000791970)
+    {
+    collSys = "AuAu";
+}
+if (beam.first.pid() == 2212 && beam.second.pid() == 2212)
+    {
+    collSys = "pp";
+    }
+            if (collSys == "pp")
+            {
+              _c["pp_130"]->fill();
+              for(auto p : hfsParticles) //loop over charged hadrons
+              {
+                      _h["c10Pt130_pp_t9"]->fill(p.pT()/GeV); 
+          
+              }
+              for(auto p : pi0UPParticles) //loop over pi0s
+              {
+		      _h["c10Pt130_pp_t7"]->fill(p.pT()/GeV);
+          _h["c10Pt130_pp_t8"]->fill(p.pT()/GeV);
+         
+              }
+            }
+          else if (collSys == "AuAu")
+          {
+             if(cent < 10.) //Check centrality of the event
       {
               _c["Cent0_10"]->fill(); //fill counter for 0-10% most central events
               for(auto p : hfsParticles) //loop over charged hadrons
               {
                       _h["ChHadronsCent0_10"]->fill(p.pT()/GeV, inv2PI/(2.*p.pT()/GeV)); //additional 1/2 factor to take into account h^(+)+h^(-)/2
+                      _h["c10Pt_AuAu130_t9"]->fill(p.pT()/GeV);
+                      _h["c10Pt_AuAu130_t12"]->fill(p.pT()/GeV);
               }
               
               for(auto p : pi0UPParticles) //loop over pi0s
               {
-                      _h["Pi0PbScCent0_10"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV));
-		      _h["Pi0PbGlCent0_10"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV));
+		      _h["Pi0PbScCent0_10"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV));
+          _h["Pi0PbGlCent0_10"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV));
+          _h["c10Pt_AuAu130_t7"]->fill(p.pT()/GeV);
+          _h["c10Pt_AuAu130_t8"]->fill(p.pT()/GeV);
+          _h["c10Pt_AuAu130_t10"]->fill(p.pT()/GeV);
+          _h["c10Pt_AuAu130_t11"]->fill(p.pT()/GeV);
               }
       }
       else if(cent >= 60. && cent < 80.) //Check centrality of the event
@@ -85,14 +153,18 @@ namespace Rivet {
               for(auto p : hfsParticles) //loop over charged hadrons
               {
                       _h["ChHadronsCent60_80"]->fill(p.pT()/GeV, inv2PI/(2.*p.pT()/GeV)); //additional 1/2 factor to take into account h^(+)+h^(-)/2
+                    _h["p80Pt130_AuAu130_t12"]->fill(p.pT()/GeV);
               }
 
               for(auto p : pi0UPParticles) //loop over pi0s
               {
                       _h["Pi0PbScCent60_80"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV));
 		      _h["Pi0PbGlCent60_80"]->fill(p.pT()/GeV, inv2PI/(p.pT()/GeV)); 
+          _h["p80Pt130_AuAu130_t10"]->fill(p.pT()/GeV);
+          _h["p80Pt130_AuAu130_t11"]->fill(p.pT()/GeV);
               }
       }
+          }
     }
 
 
@@ -106,6 +178,30 @@ namespace Rivet {
       _h["Pi0PbScCent60_80"]->scaleW(1./_c["Cent60_80"]->sumW());
       _h["Pi0PbGlCent0_10"]->scaleW(1./_c["Cent0_10"]->sumW());
       _h["Pi0PbGlCent60_80"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t7"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t8"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t9"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t10"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t11"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt_AuAu130_t12"]->scaleW(1./_c["Cent0_10"]->sumW());
+     _h["c10Pt130_pp_t7"]->scaleW(1./_c["pp_130"]->sumW());
+     _h["c10Pt130_pp_t8"]->scaleW(1./_c["pp_130"]->sumW());
+     _h["c10Pt130_pp_t9"]->scaleW(1./_c["pp_130"]->sumW());
+     _h["p80Pt130_AuAu130_t10"]->scaleW(1./_c["Cent60_80"]->sumW());
+     _h["p80Pt130_AuAu130_t11"]->scaleW(1./_c["Cent60_80"]->sumW());
+     _h["p80Pt130_AuAu130_t12"]->scaleW(1./_c["Cent60_80"]->sumW());
+      divide(_h["c10Pt_AuAu130_t7"], _h["c10Pt130_pp_t7"],_s["Raa_c010_AuAu130_t7"]);
+      divide(_h["c10Pt_AuAu130_t8"], _h["c10Pt130_pp_t8"],_s["Raa_c010_AuAu130_t8"]);
+      divide(_h["c10Pt_AuAu130_t9"], _h["c10Pt130_pp_t9"],_s["Raa_c010_AuAu130_t9"]);
+      divide(_h["c10Pt_AuAu130_t10"], _h["p80Pt130_AuAu130_t10"], _s["RCP_AuAu130_t10"]);
+      divide(_h["c10Pt_AuAu130_t11"], _h["p80Pt130_AuAu130_t11"], _s["RCP_AuAu130_t11"]);
+      divide(_h["c10Pt_AuAu130_t12"], _h["p80Pt130_AuAu130_t12"], _s["RCP_AuAu130_t12"]);
+     _s["Raa_c010_AuAu130_t7"]->scaleY(1./905.);
+     _s["Raa_c010_AuAu130_t8"]->scaleY(1./905.);
+     _s["Raa_c010_AuAu130_t9"]->scaleY(1./905.);
+     _s["RCP_AuAu130_t10"]->scaleY(1./45.);
+     _s["RCP_AuAu130_t11"]->scaleY(1./45.);
+     _s["RCP_AuAu130_t12"]->scaleY(1./45.);
     }
 
       ///@}
@@ -114,6 +210,7 @@ namespace Rivet {
       map<string, Histo1DPtr> _h;
       map<string, Profile1DPtr> _p;
       map<string, CounterPtr> _c;
+      map<string, Scatter2DPtr> _s;
       ///@}
   };
 
