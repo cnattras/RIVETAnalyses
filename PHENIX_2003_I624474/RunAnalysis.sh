105582babeed16a165ead6e4e7c6ca42bc2c4824
Nikolas1204 <105123967+Nikolas1204@users.noreply.github.com>
2023-06-16 16:23:42 -0400

    Delete PHENIX_2003_I624474 directory

--- PHENIX_2003_I624474/RunAnalysis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rivet-build RivetPHENIX_2003_I624474.so PHENIX_2003_I624474.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2003_I624474:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
