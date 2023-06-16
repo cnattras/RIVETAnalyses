121d52d6a086dca3ab3d3a1b64f97e19eac9b535
Nikolas1204 <105123967+Nikolas1204@users.noreply.github.com>
2023-06-16 16:23:59 -0400

    Delete PHENIX_2018_I1658594 directory

--- PHENIX_2018_I1658594/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rivet-build RivetPHENIX_2018_I1658594.so PHENIX_2018_I1658594.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2018_I1658594:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
-rivet-merge --pwd -o /tmp/Rivet.yoda Rivet.yoda Rivet.yoda
