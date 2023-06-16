ee7eb27f273dbe5ba3fb46eb3028197ac9f22919
Joesph Beller <95881753+josephbeller@users.noreply.github.com>
2023-06-16 16:35:58 -0400

    I619987: merging

--- PHENIX_2007_I731133/RunAnalysis.sh
+++ PHENIX_2007_I731133/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2007_I731133.so PHENIX_2007_I731133.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2007_I731133:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I731133:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
