b7df1642d4cddb99afb7ac043aba2a264b26c440
Nikolas1204 <105123967+Nikolas1204@users.noreply.github.com>
2023-06-16 16:23:01 -0400

    Delete PHENIX_2006_I731133 directory

--- PHENIX_2006_I731133/RunAnalysis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rivet-build RivetPHENIX_2006_I731133.so PHENIX_2006_I731133.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I731133:cent=GEN:beam=AUAU200 -o Rivet_Output.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
--- PHENIX_2006_I731133/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rivet-build RivetPHENIX_2006_I731133.so PHENIX_2006_I731133.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2006_I731133:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
-rivet-merge --pwd -o /tmp/Rivet.yoda Rivet.yoda Rivet.yoda
