d6b253f356a359846f1b4fa82e57abd1c44b4bca
Nikolas1204 <105123967+Nikolas1204@users.noreply.github.com>
2023-06-21 13:22:11 -0400

    Update RunAnalysis.sh

--- PHENIX_2020_I1773662/RunAnalysis.sh
+++ PHENIX_2020_I1773662/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2020_I1773662.so PHENIX_2020_I1773662.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2020_I1773662:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2020_I1773662:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
