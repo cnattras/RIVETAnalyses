300d167666be22139a67cb8892fa7618b88a968c
Joesph Beller <95881753+josephbeller@users.noreply.github.com>
2023-06-23 11:12:40 -0400

    I619063: fixed RunAnalysis script to match renamed file in Centralities/Calibration

--- STAR_2003_I619063/RunAnalysis.sh
+++ STAR_2003_I619063/RunAnalysis.sh
@@ -3,5 +3,5 @@ ANALYSIS_DIR=$PWD
 rm *.so
 export RIVET_ANALYSIS_PATH=$PWD
 rivet-build RivetSTAR_2003_I619063.so STAR_2003_I619063.cc
-rivet --pwd -p $ANALYSIS_DIR/Centrality/calibration_AuAu_130GeV_STAR.yoda -a STAR_2003_I619063:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
-rivet-merge --pwd -p $ANALYSIS_DIR/Centrality/calibration_AuAu_130GeV_STAR.yoda -o /tmp/Rivet.yoda Rivet.yoda Rivet.yoda
+rivet --pwd -p ../Centralities/Calibration/calibration_STAR_AuAu130GeV.yoda -a STAR_2003_I619063:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet-merge --pwd -p ../Centralities/Calibration/calibration_AuAu_130GeV_STAR.yoda -o /tmp/Rivet.yoda Rivet.yoda Rivet.yoda
