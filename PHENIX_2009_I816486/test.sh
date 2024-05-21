e72b8155eff5f1ad6707f0cad76f2fe27d392bb1
josiehakanson <bhakans1@vols.utk.edu>
2024-05-21 16:07:24 -0400

    new .plot file

--- PHENIX_2009_I816486/test.sh
+++
@@ -1,7 +0,0 @@
-
-#This compiles the code
-rivet-build RivetPHENIX_2009_I816486.so PHENIX_2009_I816486.cc
-#This runs it
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2009_I816486:cent=GEN:beam=AUAU -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
-#rivet-mkhtml --pwd Rivet.yoda
-rivet-merge --pwd -o /tmp/Rivet.yoda Rivet.yoda Rivet.yoda
