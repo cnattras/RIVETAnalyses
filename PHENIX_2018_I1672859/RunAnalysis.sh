b958cb96f10fe26a3ea744fea51779ae12fe3e89
josiehakanson <bhakans1@vols.utk.edu>
2024-05-24 14:17:49 -0400

    Updated for isaac

--- PHENIX_2018_I1672859/RunAnalysis.sh
+++ PHENIX_2018_I1672859/RunAnalysis.sh
@@ -2,5 +2,4 @@
 rm PHENIX_2018_I1672859.so
 rivet-build RivetPHENIX_2018_I1672859.so PHENIX_2018_I1672859.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2018_I1672859:cent=GEN:beam=CUAU200 -o Rivet_CuAu.yoda /phenix/scratch/cen/sampleHepData/CuAu200/hepmcCu.Au.200GeV.1000Events.seed0.pthard30to40.hepmc.gz
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2018_I1672859:cent=GEN:beam=PP200 -o Rivet_pp.yoda /phenix/scratch/cen/sampleHepData/pp200/hepmcd.Au.200GeV.30000Events.seed11.pthard30to40.hepmc.gz
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2018_I1672859:cent=GEN:beam=CUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
