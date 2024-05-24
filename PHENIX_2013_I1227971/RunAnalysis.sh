61afed1773052a050d0a1206dc2ccf0b22e1e59f
josiehakanson <bhakans1@vols.utk.edu>
2024-05-24 12:36:01 -0400

    latex error

--- PHENIX_2013_I1227971/RunAnalysis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rivet-build RivetPHENIX_2013_I1227971.so PHENIX_2013_I1227971.cc
-export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2013_I1227971:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
