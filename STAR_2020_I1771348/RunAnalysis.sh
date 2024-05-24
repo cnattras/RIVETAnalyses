1546fa444297575aa3306f0f7b7628991b1eef2a
josiehakanson <bhakans1@vols.utk.edu>
2024-05-24 16:29:53 -0400

    updated RunAnalysis.sh

--- STAR_2020_I1771348/RunAnalysis.sh
+++ STAR_2020_I1771348/RunAnalysis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 rivet-build RivetSTAR_2020_I1771348.so STAR_2020_I1771348.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p calibration_AuAu_200GeV_STAR.yoda -a STAR_2020_I1771348:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p calibration_AuAu_200GeV_STAR.yoda -a STAR_2020_I1771348:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+
+# Changed dAU to AUAU (because of calibration AuAu)
+# Josie Hakanson 5-24-2024
