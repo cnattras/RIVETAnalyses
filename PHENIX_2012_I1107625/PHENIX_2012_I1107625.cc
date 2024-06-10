dda1a8248e38766ed284400d876813072cdb79b0
Olivia Bartoshesky <obartosh@vols.utk.edu>
2024-06-10 11:00:33 -0400

    bin shift correction added

--- PHENIX_2012_I1107625/PHENIX_2012_I1107625.cc
+++ PHENIX_2012_I1107625/PHENIX_2012_I1107625.cc
@@ -25,6 +25,32 @@ namespace Rivet {
     /// Constructor
     DEFAULT_RIVET_ANALYSIS_CTOR(PHENIX_2012_I1107625);
 
+//create binShift function
+    void binShift(YODA::Histo1D& histogram) {
+        std::vector<YODA::HistoBin1D> binlist = histogram.bins();
+        int n = 0;
+        for (YODA::HistoBin1D bins : binlist) {
+            double p_high = bins.xMax();
+            double p_low = bins.xMin();
+            //Now calculate f_corr
+            if (bins.xMin() == binlist[0].xMin()) { //Check if we are working with first bin
+                float b = 1 / (p_high - p_low) * log(binlist[0].height()/binlist[1].height());
+                float f_corr = -b * (p_high - p_low) * pow(M_E, -b * (p_high+p_low) / 2) / (pow(M_E, -b * p_high) - pow(M_E, -b*p_low));
+                histogram.bin(n).scaleW(f_corr);
+                n += 1;
+            } else if (bins.xMin() == binlist.back().xMin()){ //Check if we are working with last bin
+                float b = 1 / (p_high - p_low) * log(binlist[binlist.size()-2].height() / binlist.back().height());
+                float f_corr = -b * (p_high - p_low) * pow(M_E, -b * (p_high+p_low) / 2) / (pow(M_E, -b * p_high) - pow(M_E, -b*p_low));
+                histogram.bin(n).scaleW(f_corr);
+            } else { //Check if we are working with any middle bin
+                float b = 1 / (p_high - p_low) * log(binlist[n-1].height() / binlist[n+1].height());
+                float f_corr = -b * (p_high - p_low) * pow(M_E, -b * (p_high+p_low) / 2) / (pow(M_E, -b * p_high) - pow(M_E, -b*p_low));
+                histogram.bin(n).scaleW(f_corr);
+                n += 1;
+            }
+        }
+    }
+    
     void init() {
       //std::initializer_list<int> pdgIds = {111};  // Pion 0
 
@@ -379,6 +405,50 @@ namespace Rivet {
     }
 
     void finalize() {
+
+        binShift(*hPion0Pt["ptyields39c10"]);
+        binShift(*hPion0Pt["ptyields39c20"]);
+        binShift(*hPion0Pt["ptyields39c40"]);
+        binShift(*hPion0Pt["ptyields39c60"]);
+        binShift(*hPion0Pt["ptyields39c86"]);
+        binShift(*hPion0Pt["ptyields39call"]);
+
+        binShift(*hPion0Pt["ptyields62c10"]);
+        binShift(*hPion0Pt["ptyields62c20"]);
+        binShift(*hPion0Pt["ptyields62c40"]);
+        binShift(*hPion0Pt["ptyields62c60"]);
+        binShift(*hPion0Pt["ptyields62c86"]);
+        binShift(*hPion0Pt["ptyields62call"]);
+
+        binShift(*hPion0Pt["c10Pt_AuAu39"]);
+        binShift(*hPion0Pt["c20Pt_AuAu39"]);
+        binShift(*hPion0Pt["c40Pt_AuAu39"]);
+        binShift(*hPion0Pt["c60Pt_AuAu39"]);
+        binShift(*hPion0Pt["c86Pt_AuAu39"]);
+        binShift(*hPion0Pt["callPt_AuAu39"]);
+
+        binShift(*hPion0Pt["c10Pt_AuAu62"]);
+        binShift(*hPion0Pt["c20Pt_AuAu62"]);
+        binShift(*hPion0Pt["c40Pt_AuAu62"]);
+        binShift(*hPion0Pt["c60Pt_AuAu62"]);
+        binShift(*hPion0Pt["c86Pt_AuAu62"]);
+        binShift(*hPion0Pt["callPt_AuAu62"]);
+
+        binShift(*hPion0Pt["c10Pt39_pp"]);
+        binShift(*hPion0Pt["c20Pt39_pp"]);
+        binShift(*hPion0Pt["c40Pt39_pp"]);
+        binShift(*hPion0Pt["c60Pt39_pp"]);
+        binShift(*hPion0Pt["c86Pt39_pp"]);
+        binShift(*hPion0Pt["callPt39_pp"]);
+
+        binShift(*hPion0Pt["c10Pt62_pp"]);
+        binShift(*hPion0Pt["c20Pt62_pp"]);
+        binShift(*hPion0Pt["c40Pt62_pp"]);
+        binShift(*hPion0Pt["c60Pt62_pp"]);
+        binShift(*hPion0Pt["c86Pt62_pp"]);
+        binShift(*hPion0Pt["callPt62_pp"]);
+
+ 
 /*
       bool AuAu39_available = false;
       bool AuAu62_available = false;
