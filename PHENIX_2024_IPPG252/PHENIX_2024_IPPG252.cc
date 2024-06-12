91e014d99be1d9bd3f6dc37349559900b02b6e5d
Olivia Bartoshesky <obartosh@vols.utk.edu>
2024-06-12 15:28:58 -0400

    added books, added beam

--- PHENIX_2024_IPPG252/PHENIX_2024_IPPG252.cc
+++ PHENIX_2024_IPPG252/PHENIX_2024_IPPG252.cc
@@ -23,12 +23,24 @@ namespace Rivet {
     /// Book histograms and initialise projections before the run
     void init() {
 
-      // Initialise and register projections
+// Initialise and register projections
+      const FinalState fs(Cuts::abseta < 4.9);
+        //In case "NONE" is given as option
+      const ParticlePair& beam = beams();
 
+      beamOpt = getOption<string>("beam","NONE");
+        
+
+      if (beamOpt == "NONE") {
+      if (beam.first.pid() == 2212 && beam.second.pid() == 2212) collSys = pp;
+      }
+
+      if (beamOpt =="pp") collSys = pp;
+      
       // The basic final-state projection:
       // all final-state particles within
       // the given eta acceptance
-      const FinalState fs(Cuts::abseta < 4.9);
+      
 
       // The final-state particles declared above are clustered using FastJet with
       // the anti-kT algorithm and a jet-radius parameter 0.4
@@ -51,13 +63,35 @@ namespace Rivet {
 
       // Book histograms
       // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
+     
       // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
+      book(h["jetpt"], 1, 1, 1);
+      book(h["jetcross"], 1, 1, 2);
+
+      book(h["zg910"], 2, 1, 1);
+      book(h["zg1012"], 2, 1, 2);
+      book(h["zg1214"], 2, 1, 3);
+      book(h["zg1417"], 2, 1, 4);
+      book(h["zg1720"], 2, 1, 5);
+      book(h["zg2024"], 2, 1, 6);
+      book(h["zg2429"], 2, 1, 7);
+           
+      book(h["ETA910"], 3, 1, 1);
+
+      book(h["ETA1012"], 4, 1, 1);
+      book(h["ETA1214"], 4, 1, 2);
+      book(h["ETA1217"], 4, 1, 3);
+      book(h["ETA1720"], 4, 1, 4);
+      book(h["ETA2024"], 4, 1, 5);
+      book(h["ETA2429"], 4, 1, 6);
+
+      book(h["R910"], 5, 1, 1);
+      book(h["R1012"], 5, 1, 1);
+      book(h["R1214"], 5, 1, 1);
+      book(h["R1417"], 5, 1, 1);
+      book(h["R1720"], 5, 1, 1);
+      book(h["R2024"], 5, 1, 1);
+      book(h["R2429"], 5, 1, 1);
 
     }
 
@@ -86,7 +120,7 @@ namespace Rivet {
       if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
 
       // Fill histogram with leading b-jet pT
-      _h["XXXX"]->fill(bjets[0].pT()/GeV);
+      h["XXXX"]->fill(bjets[0].pT()/GeV);
 
     }
 
@@ -94,9 +128,9 @@ namespace Rivet {
     /// Normalise histograms etc., after the run
     void finalize() {
 
-      normalize(_h["XXXX"]); // normalize to unity
-      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in pb (no cuts)
-      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
+      normalize(h["XXXX"]); // normalize to unity
+      normalize(h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in pb (no cuts)
+      scale(h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
 
     }
 
@@ -105,9 +139,12 @@ namespace Rivet {
 
     /// @name Histograms
     /// @{
-    map<string, Histo1DPtr> _h;
-    map<string, Profile1DPtr> _p;
-    map<string, CounterPtr> _c;
+    map<string, Histo1DPtr> h;
+    map<string, Profile1DPtr> p;
+    map<string, CounterPtr> c;
+    string beamOpt;
+    enum CollisionSystem {pp};
+    CollisionSystem collSys;
     /// @}
 
 
