7be5a9f422099c284b32645c58a30c40a80f68c7
Aidan Christopher Hill <ahill117@vols.utk.edu>
2024-06-14 10:20:28 -0400

    added beam to command so analysis would run

--- PHENIX_2010_I857187/RunAnalysis.sh
+++ PHENIX_2010_I857187/RunAnalysis.sh
@@ -2,5 +2,5 @@
 rm RivetPHENIX_2010_I857187.so
 rivet-build RivetPHENIX_2010_I857187.so PHENIX_2010_I857187.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2010_I857187:cent=GEN -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2010_I857187:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
 
