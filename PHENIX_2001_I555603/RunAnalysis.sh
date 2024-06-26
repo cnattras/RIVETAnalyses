c9bedc7524b5f7229d38c0610c1408c89bf9add4
Aidan Christopher Hill <ahill117@vols.utk.edu>
2024-06-26 14:30:36 -0400

    updating executable

--- PHENIX_2001_I555603/RunAnalysis.sh
+++ PHENIX_2001_I555603/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2001_I555603.so PHENIX_2001_I555603.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu130GeV.yoda -a PHENIX_2001_I555603:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu130GeV.yoda -a PHENIX_2001_I555603:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
