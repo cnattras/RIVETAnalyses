99bbded2bbbc292078d93a729cd227b1c3e048aa
Iftekhar Ahmed <iahmed11@login2.cn.isaac.utk.edu>
2024-06-27 15:38:58 -0400

    Fixed analyze sction

--- PHENIX_2016_I1394433/PHENIX_2016_I1394433.cc
+++ PHENIX_2016_I1394433/PHENIX_2016_I1394433.cc
@@ -41,7 +41,8 @@ namespace Rivet {
             if (beam.first.pid() == 1000791970 && beam.second.pid() == 1000791970)
       {
           NN = 197.;
-          if (fuzzyEquals(sqrtS()/GeV, 7.7*NN, 1E-3)) collSys = AuAu7;
+          if (fuzzyEquals(sqrtS()/GeV, 7.7*
+          NN, 1E-3)) collSys = AuAu7;
           if (fuzzyEquals(sqrtS()/GeV, 14.5*NN, 1E-3)) collSys = AuAu14;
           if (fuzzyEquals(sqrtS()/GeV, 19.6*NN, 1E-3)) collSys = AuAu19;
           if (fuzzyEquals(sqrtS()/GeV, 27*NN, 1E-3)) collSys = AuAu27;
@@ -66,7 +67,7 @@ namespace Rivet {
       }
       if (beam.first.pid() == 1000010020 && beam.second.pid() == 1000791970)
       {
-          if (fuzzyEquals(sqrtS()/GeV, 200*NN, 1E-3)) collSys = DAu200;
+          if (fuzzyEquals(sqrtS()/GeV, 200*NN, 1E-3)) collSys = dAu200;
       }
       if (beam.first.pid() == 1000020030 && beam.second.pid() == 1000791970)
       {
@@ -82,49 +83,49 @@ namespace Rivet {
         
      //Au+Au collisions
 
-        book(_h["hist_E_200"], 1, 1, 3);
-        book(_h["hist_Ch_200"], 2, 1, 3);
-        book(_h["hist_E_130"], 3, 1, 3);
-        book(_h["hist_Ch_130"], 4, 1, 3);
-        book(_h["hist_E_62.4"], 5, 1, 3);
-        book(_h["hist_Ch_62.4"], 6, 1, 3);
-        book(_h["hist_E_39"], 7, 1, 3);
-        book(_h["hist_Ch_39"], 8, 1, 3);
-        book(_h["hist_E_27"],9,1,3);
-        book(_h["hist_Ch_27"],10,1,3);
-        book(_h["hist_E_19.6"],11,1,3);
-        book(_h["hist_Ch_19.6"],12,1,3);
-        book(_h["hist_E_14.5"],13,1,3);
-        book(_h["hist_Ch_14.5"],14,1,3);
-        book(_h["hist_E_7.7"],15,1,3);
-        book(_h["hist_Ch_7.7"],16,1,3);
+        book(_hist_AuAu_E_200, "d01-x01-y03", refData(1, 1, 3));
+        book(_hist_AuAu_Ch_200, "d02-x01-y03", refData(2, 1, 3));
+        book(_hist_AuAu_E_130, "d03-x01-y03", refData (3, 1, 3));
+        book(_hist_AuAu_Ch_130, "d04-x01-y03", refData (4, 1, 3));
+        book(_hist_AuAu_E_62, "d05-x01-y03", refData( 5, 1, 3));
+        book(_hist_AuAu_Ch_62, "d06-x01-y03", refData (6, 1, 3));
+        book(_hist_AuAu_E_39, "d07-x01-y03", refData (7, 1, 3));
+        book(_hist_AuAu_Ch_39, "d08-x01-y03", refData (8, 1, 3));
+        book(_hist_AuAu_E_27, "d09-x01-y03", refData(9,1,3));
+        book(_hist_AuAu_Ch_27, "d10-x01-y03", refData(10,1,3));
+        book(_hist_AuAu_E_19, "d11-x01-y03", refData(11,1,3));
+        book(_hist_AuAu_Ch_19, "d12-x01-y03", refData(12,1,3));
+        book(_hist_AuAu_E_14, "d13-x01-y03", refData(13,1,3));
+        book(_hist_AuAu_Ch_14, "d14-x01-y03", refData(14,1,3));
+        book(_hist_AuAu_E_7, "d15-x01-y03", refData(15,1,3));
+        book(_hist_AuAu_Ch_7, "d16-x01-y03", refData(16,1,3));
       
      //Cu+Cu collisions   
       
-        book(_h["hist_E_200_Cu"],17,1,3);
-        book(_h["hist_Ch_200_Cu"],18,1,3);
-        book(_h["hist_E_62.4_Cu"],19,1,3);
-        book(_h["hist_Ch_62.4_Cu"],20,1,3);
+        book(_hist_CuCu_E_200, "d17-x01-y03", refData(17,1,3));
+        book(_hist_CuCu_Ch_200, "d18-x01-y03", refData(18,1,3));
+        book(_hist_CuCu_E_62, "d19-x01-y03", refData(19,1,3));
+        book(_hist_CuCu_Ch_62, "d20-x01-y03", refData(20,1,3));
 
     //Cu+Au collisions
 
-        book(_h["hist_E_200_Cu_Au"],21,1,3);
-        book(_h["hist_Ch_200_Cu_Au"],22,1,3);
+        book(_hist_CuAu_E_200, "d21-x01-y03", refData(21,1,3));
+        book(_hist_CuAu_Ch_200, "d22-x01-y03", refData(22,1,3));
 
     //U+U collisions
 
-        book(_h["hist_E_193_UU"],23,1,3);
-        book(_h["hist_Ch_193_UU"],24,1,3);
+        book(_hist_UU_E_193, "d23-x01-y03", refData(23,1,3));
+        book(_hist_UU_Ch_193, "d24-x01-y03", refData(24,1,3));
 
     //d+Au collisions
 
-        book(_h["hist_E_200_dAu"],25,1,3);
-        book(_h["hist_Ch_200_dAu"],26,1,3);
+        book(_hist_dAu_E_200, "d25-x01-y03", refData(25,1,3));
+        book(_hist_dAu_Ch_200, "d26-x01-y03", refData(26,1,3));
 
     //He+Au collisions
     
-        book(_h["hist_E_200_He_A"],27,1,3);
-        book(_h["hist_Ch_200_He_A"],28,1,3);
+        book(_hist_HeAu_E_200, "d27-x01-y03", refData(27,1,3));
+        book(_hist_HeAu_Ch_200, "d28-x01-y03", refData(28,1,3));
       
         
      declare(ALICE::PrimaryParticles(Cuts::abseta < 0.35 && Cuts::pT > 0.0*MeV && Cuts::abscharge > 0), "APRIM");
@@ -134,22 +135,13 @@ namespace Rivet {
     void analyze(const Event& event)
     {
         const ParticlePair& beam = beams();
-        double NN = 0.;
-        string thisbeam = "Empty";
+        
         
         //track beams
-        if (beam.first.pid() == 1000791970 && beam.second.pid() == 1000791970)
-        {
-          thisbeam = "AuAu";
-          NN = 197.;
-          if (fuzzyEquals(sqrtS()/GeV, 200*NN, 1E-3)) thisbeam += "200GeV";
-          if (fuzzyEquals(sqrtS()/GeV, 62.4*NN, 1E-3)) thisbeam += "62GeV";
-          if (fuzzyEquals(sqrtS()/GeV, 39*NN, 1E-3)) thisbeam += "39GeV";
-        }
+
         
-        int nch200counter = 0; int nch62counter = 0;
-        int nch39counter = 0; double sumET200 = 0.;
-        double sumET62 = 0.; double sumET39 = 0.;
+        int nchcounter = 0;  
+        double sumET = 0.; 
         
         //get charged particles
         Particles chargedParticles = applyProjection<ALICE::PrimaryParticles>(event,"APRIM").particles();
@@ -162,45 +154,97 @@ namespace Rivet {
         //loop over particles to get total charged particles & Et
         for(const Particle& p : chargedParticles)
         {
-            const int id = abs(p.pid());
-            if(id==11 || id==13) continue; //exclude electron & muon
-            
-            if(thisbeam == "AuAu200GeV")
-            {
-                nch200counter ++;
-                sumET200 += p.Et()/GeV;
-            }
-            else if(thisbeam == "AuAu62GeV")
-            {
-                nch62counter ++;
-                sumET62 += p.Et()/GeV;
-            }
-            else if(thisbeam == "AuAu39GeV")
-            {
-                nch39counter ++;
-                sumET39 += p.Et()/GeV;
-            }
+                nchcounter ++;
+                sumET += p.Et()/GeV;
         }
         
         
-        //fill hists
-        if(thisbeam == "AuAu200GeV")
+        //fill hists for AuAU collisions
+        if(collSys == AuAu200)
         {
-            _hist_Ch_200->fill(c,nch200counter/0.7);
-            _hist_E_200->fill(c,sumET200/0.7);
+            _hist_AuAu_Ch_200->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_200->fill(c,sumET/0.7);
         }
-        else if(thisbeam == "AuAu62GeV")
+        else if(collSys == AuAu130)
         {
-            _hist_Ch_62->fill(c,nch62counter/0.7);
-            _hist_E_62->fill(c,sumET62/0.7);
+            _hist_AuAu_Ch_130->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_130->fill(c,sumET/0.7);
         }
-        else if(thisbeam == "AuAu39GeV")
+        else if(collSys == AuAu62)
         {
-            _hist_Ch_39->fill(c,nch39counter/0.7);
-            _hist_E_39->fill(c,sumET39/0.7);
+            _hist_AuAu_Ch_62->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_62->fill(c,sumET/0.7);
         }
-        
- 
+        else if(collSys == AuAu39)
+        {
+            _hist_AuAu_Ch_39->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_39->fill(c,sumET/0.7);
+        }
+        else if(collSys == AuAu27)
+        {
+            _hist_AuAu_Ch_27->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_27->fill(c,sumET/0.7);
+        }
+        else if(collSys == AuAu19)
+        {
+            _hist_AuAu_Ch_19->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_19->fill(c,sumET/0.7);
+        }
+        else if(collSys == AuAu14)
+        {
+            _hist_AuAu_Ch_14->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_14->fill(c,sumET/0.7);
+        }
+        else if(collSys == AuAu7)
+        {
+            _hist_AuAu_Ch_7->fill(c,nchcounter/0.7);
+            _hist_AuAu_E_7->fill(c,sumET/0.7);
+        }
+
+        //fill hists for CuCu collisions
+
+        if(collSys == CuCu200)
+        {
+            _hist_CuCu_Ch_200->fill(c,nchcounter/0.7);
+            _hist_CuCu_E_200->fill(c,sumET/0.7);
+        }
+        else if(collSys == CuCu62)
+        {
+            _hist_CuCu_Ch_62->fill(c,nchcounter/0.7);
+            _hist_CuCu_E_62->fill(c,sumET/0.7);
+        }
+
+         //fill hists for CuAu collisions
+
+         if(collSys == CuAu200)
+        {
+            _hist_CuAu_Ch_200->fill(c,nchcounter/0.7);
+            _hist_CuAu_E_200->fill(c,sumET/0.7);
+        }
+
+        //fill hists for UU collisions
+
+        if(collSys == UU193)
+        {
+            _hist_UU_Ch_193->fill(c,nchcounter/0.7);
+            _hist_UU_E_193->fill(c,sumET/0.7);
+        }
+
+        //fill hists for dAu collisions
+
+        if(collSys == dAu200)
+        {
+            _hist_dAu_Ch_200->fill(c,nchcounter/0.7);
+            _hist_dAu_E_200->fill(c,sumET/0.7);
+        }
+
+        //fill hists for HeAu collisions
+
+        if(collSys == HeAu200)
+        {
+            _hist_HeAu_Ch_200->fill(c,nchcounter/0.7);
+            _hist_HeAu_E_200->fill(c,sumET/0.7);
+        }
     }
 
     void finalize()
@@ -216,16 +260,49 @@ namespace Rivet {
     map<string, Histo1DPtr> _h;
     map<string, Profile1DPtr> _p;
     map<string, CounterPtr> _c;
-    Profile1DPtr _hist_Ch_200;
-    Profile1DPtr _hist_Ch_62;
-    Profile1DPtr _hist_Ch_39;
-    Profile1DPtr _hist_E_200;
-    Profile1DPtr _hist_E_62;
-    Profile1DPtr _hist_E_39;
 
+    //AuAu collision
+    Profile1DPtr _hist_AuAu_Ch_200;
+    Profile1DPtr _hist_AuAu_Ch_130;
+    Profile1DPtr _hist_AuAu_Ch_62;
+    Profile1DPtr _hist_AuAu_Ch_39;
+    Profile1DPtr _hist_AuAu_Ch_27;
+    Profile1DPtr _hist_AuAu_Ch_19;
+    Profile1DPtr _hist_AuAu_Ch_14;
+    Profile1DPtr _hist_AuAu_Ch_7;
+    Profile1DPtr _hist_AuAu_E_200;
+    Profile1DPtr _hist_AuAu_E_130;
+    Profile1DPtr _hist_AuAu_E_62;
+    Profile1DPtr _hist_AuAu_E_39;
+    Profile1DPtr _hist_AuAu_E_27;
+    Profile1DPtr _hist_AuAu_E_19;
+    Profile1DPtr _hist_AuAu_E_14;
+    Profile1DPtr _hist_AuAu_E_7;
+
+    //CuCu collision
+    Profile1DPtr _hist_CuCu_Ch_200;
+    Profile1DPtr _hist_CuCu_Ch_62;
+    Profile1DPtr _hist_CuCu_E_200;
+    Profile1DPtr _hist_CuCu_E_62;
+
+    //CuAu collision
+    Profile1DPtr _hist_CuAu_Ch_200;
+    Profile1DPtr _hist_CuAu_E_200;
+
+    //UU collision
+    Profile1DPtr _hist_UU_Ch_193;
+    Profile1DPtr _hist_UU_E_193;
+
+    //dAu collision
+    Profile1DPtr _hist_dAu_Ch_200;
+    Profile1DPtr _hist_dAu_E_200;
+
+    //HeAu collision
+    Profile1DPtr _hist_HeAu_Ch_200;
+    Profile1DPtr _hist_HeAu_E_200;
     //@}
     string beamOpt;
-    enum CollisionSystem {AuAu7,AuAu14,AuAu19,AuAu27,AuAu39,AuAu62,AuAu130,AuAu200,CuCu62,CuCu200,CuAu200,UU193,DAu200,HeAu200,pp200};
+    enum CollisionSystem {AuAu7,AuAu14,AuAu19,AuAu27,AuAu39,AuAu62,AuAu130,AuAu200,CuCu62,CuCu200,CuAu200,UU193,dAu200,HeAu200,pp200};
     CollisionSystem collSys;
 
 
