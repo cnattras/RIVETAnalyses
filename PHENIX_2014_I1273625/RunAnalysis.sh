e45cdea4e4f8c3929ffacf674f9fd6ef13713b3d
Olivia Bartoshesky <obartosh@vols.utk.edu>
2024-07-01 11:25:57 -0400

    updated run analysis

--- PHENIX_2014_I1273625/RunAnalysis.sh
+++ PHENIX_2014_I1273625/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2014_I1273625.so PHENIX_2014_I1273625.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2014_I1273625:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2014_I1273625:cent=GEN:beam=AUAU200 -a PHENIX_2014_I1273625:cent=GEN:beam=AUAU130 -a PHENIX_2014_I1273625:cent=GEN:beam=AUAU62 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
