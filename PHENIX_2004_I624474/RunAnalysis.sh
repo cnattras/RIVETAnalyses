eea5f536ded55fa877cfe84bdfb57c535b8ad6e7
josiehakanson <135037471+josiehakanson@users.noreply.github.com>
2024-07-18 15:05:10 -0400

    Update RunAnalysis.sh

--- PHENIX_2004_I624474/RunAnalysis.sh
+++ PHENIX_2004_I624474/RunAnalysis.sh
@@ -2,4 +2,4 @@
 rivet-build RivetPHENIX_2004_I624474.so PHENIX_2004_I624474.cc
 export RIVET_ANALYSIS_PATH=$PWD
 #rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2004_I624474:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu130GeV.yoda -a PHENIX_2004_I624474:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2004_I624474:cent=GEN:beam=AUAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
