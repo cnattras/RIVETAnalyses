17bc770ba772c7d2b2e14486d057e22dc1b3f1fd
josiehakanson <135037471+josiehakanson@users.noreply.github.com>
2024-07-22 13:30:11 -0400

    Update PHENIX_2004_I624474.cc

--- PHENIX_2004_I624474/PHENIX_2004_I624474.cc
+++ PHENIX_2004_I624474/PHENIX_2004_I624474.cc
@@ -1900,8 +1900,8 @@ namespace Rivet {
 		binShift(*hPiPi["AUAU60_92Piminus"]);
 		binShift(*hPiPi["AUAU60_92Piplus"]);
 
-		binShift(*hPiPi["AUAU0_5Kminus"]);
-		binShift(*hPiPi["AUAU0_5Kplus"]);
+		//binShift(*hPiPi["AUAU0_5Kminus"]);
+		//binShift(*hPiPi["AUAU0_5Kplus"]);
 		
 		
 
