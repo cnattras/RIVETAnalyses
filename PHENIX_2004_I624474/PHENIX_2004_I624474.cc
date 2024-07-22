bec33a6516627851b1af04a14089e4942c65d2ae
josiehakanson <135037471+josiehakanson@users.noreply.github.com>
2024-07-22 12:27:02 -0400

    Update PHENIX_2004_I624474.cc

--- PHENIX_2004_I624474/PHENIX_2004_I624474.cc
+++ PHENIX_2004_I624474/PHENIX_2004_I624474.cc
@@ -1800,8 +1800,8 @@ namespace Rivet {
 	void finalize() {
 		binShift(*hAUAU_Yields["Piplusmin"]);
 		binShift(*hAUAU_Yields["Piplus5"]);
-		/*binShift(*hAUAU_Yields["Piplus10"]);
-		binShift(*hAUAU_Yields["Piplus15"]);
+		binShift(*hAUAU_Yields["Piplus10"]);
+		/*binShift(*hAUAU_Yields["Piplus15"]);
 		binShift(*hAUAU_Yields["Piplus20"]);
 		binShift(*hAUAU_Yields["Piplus30"]);
 		binShift(*hAUAU_Yields["Piplus40"]);
