421b6a051a98726818dd85ccafa73bc40d4947b6
Christine Nattrass <cnattras@cern.ch>
2025-07-17 16:56:17 -0400

    making scripts more useful

--- SPHENIX_2025_I2907537/RunFIFOAuAu200GeV.sh
+++ SPHENIX_2025_I2907537/RunFIFOAuAu200GeV.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-#mkfifo /tmp/fifo.hepmc 
-#export RIVET_ANALYSIS_PATH=/tmp
-#/usr/local/share/Pythia8/examples/./main144 -c /work/AuAu200.cmnd -n 100 -o /tmp/fifo.hepmc & \
-#rivet --pwd -a RHIC_2019_CentralityCalibration /tmp/fifo.hepmc
-#!/bin/bash
-#mkfifo /tmp/fifo.hepmc
-#export RIVET_ANALYSIS_PATH=/tmp
-#/usr/local/share/Pythia8/examples/./main144 -c /work/AuAu200.cmnd -n 100 -o /tmp/fifo.hepmc &
-#pythia8-main144 -c /work/AuAu200.cmnd -o /tmp/fifo.hepmc
-#MAIN_PID=$!
-#rivet --pwd -a RHIC_2019_CentralityCalibration /tmp/fifo.hepmc
-#wait $MAIN_PID
-#rm /tmp/fifo.hepmc
+
+
+rivet-build RivetSPHENIX_2025_I2907537.so SPHENIX_2025_I2907537.cc
+export RIVET_ANALYSIS_PATH=$PWD
 
 mkfifo AuAu200GeV.hepmc 
 cat AuAu200GeV.hepmc > /dev/null &
