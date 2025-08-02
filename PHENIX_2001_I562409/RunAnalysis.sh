aebe2b5b95b4741eb6fc0a1dc23ef32ed169327f
Christine Nattrass <cnattras@cern.ch>
2025-08-01 20:00:56 -0400

    updating calibration file

--- PHENIX_2001_I562409/RunAnalysis.sh
+++ PHENIX_2001_I562409/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2001_I562409.so PHENIX_2001_I562409.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu200GeV.yoda -a PHENIX_2001_I562409:cent=GEN:beam=dAU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/AuAu130GeV.yoda -a PHENIX_2001_I562409:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
