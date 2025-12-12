9688e2a5562f3a0e3db696a3e747f26d934ce4d1
Christine Nattrass <cnattras@cern.ch>
2025-12-12 10:51:29 -0500

    makng code crash if it does not compile

--- PHENIX_2025_I2820229/RunAnalysis.sh
+++ PHENIX_2025_I2820229/RunAnalysis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm RivetPHENIX_2025_I2820229.so
 rivet-build RivetPHENIX_2025_I2820229.so PHENIX_2025_I2820229.cc
 export RIVET_ANALYSIS_PATH=$PWD
 rivet --pwd -a PHENIX_2025_I2820229 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
