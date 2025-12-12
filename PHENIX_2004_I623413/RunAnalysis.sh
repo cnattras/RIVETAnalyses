1a1302a65bc4a2933512b9ff3ebe93509713bc47
Christine Nattrass <cnattras@cern.ch>
2025-12-12 14:05:44 -0500

    adding line to remove library file

--- PHENIX_2004_I623413/RunAnalysis.sh
+++ PHENIX_2004_I623413/RunAnalysis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm RivetPHENIX_2004_I623413.so
 rivet-build RivetPHENIX_2004_I623413.so PHENIX_2004_I623413.cc
 export RIVET_ANALYSIS_PATH=$PWD
 rivet --pwd -p ../Centralities/Calibration/AuAu130GeV.yoda -a PHENIX_2004_I623413:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
