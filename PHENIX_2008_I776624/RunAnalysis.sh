0765f6133b7a943f7723565b773e121a584acd37
Christine Nattrass <cnattras@cern.ch>
2025-12-17 11:29:59 -0500

    fixing script

--- PHENIX_2008_I776624/RunAnalysis.sh
+++ PHENIX_2008_I776624/RunAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rivet-build RivetPHENIX_2008_I776624.so PHENIX_2008_I776624.cc
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_CuCu200GeV.yoda -a PHENIX_2008_I776624:cent=GEN:beam=CUCU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+rivet --pwd -p ../Centralities/Calibration/CuCu200GeV.yoda -a PHENIX_2008_I776624:cent=GEN:beam=CUCU200 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
