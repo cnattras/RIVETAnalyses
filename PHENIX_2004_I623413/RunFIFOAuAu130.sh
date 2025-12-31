04e1d0eeeffedd61cb26d2e1de944dd88a085f39
Artor101 <artor101@gmail.com>
2025-12-31 16:38:21 +0000

    Removed Rivet Generated Files and Adding Code updates

--- PHENIX_2004_I623413/RunFIFOAuAu130.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#mkfifo /tmp/fifo.hepmc 
-
-mkfifo AuAu130GeV.hepmc 
-cat AuAu130GeV.hepmc > /dev/null &
-pythia8-main144 -c ../RunPYTHIAInDocker/AuAu130GeV.cmnd -c main144HepMC.cmnd -c main144RivetAuAu130.cmnd -o AuAu130GeV
-pkill -f "cat AuAu130GeV.hepmc"
-rm -f AuAu130GeV.hepmc
