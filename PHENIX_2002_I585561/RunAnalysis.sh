e451ad93319080b9bcb9e4c338d4f6a74df0095e
aberry46 <alexanderstevenberry@gmail.com>
2026-04-06 13:59:37 -0400

    Update RunAnalysis.sh

--- PHENIX_2002_I585561/RunAnalysis.sh
+++ PHENIX_2002_I585561/RunAnalysis.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
+
 rivet-build RivetPHENIX_2002_I585561.so PHENIX_2002_I585561.cc
+
 export RIVET_ANALYSIS_PATH=$PWD
-rivet --pwd -p ../Centralities/Calibration/calibration_PHENIX_AuAu130GeV.yoda --ignore-beams -a PHENIX_2002_I585561:cent=GEN:beam=AUAU130 -o Rivet.yoda ../testfiles/PYTHIAAuAuFileSMALLTEST.dat
+export PYTHONPATH="$(rivet-config --pythonpath):${PYTHONPATH}"
+export LD_LIBRARY_PATH="$(rivet-config --libdir):${LD_LIBRARY_PATH}"
+
+rm -f AuAu130GeV.hepmc
+mkfifo AuAu130GeV.hepmc
+cat AuAu130GeV.hepmc > /dev/null &
+
+pythia8-main144 -c ../RunPYTHIAInDocker/AuAu130GeV.cmnd -c ../RunPYTHIAInDocker/main144HepMC.cmnd -c main144Rivet.cmnd -o AuAu130GeV
+
+pkill -f "cat AuAu130GeV.hepmc" || true
+rm -f AuAu130GeV.hepmc
