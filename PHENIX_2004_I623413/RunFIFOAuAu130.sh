602436bbb7851fa58417f3b4c8dd753f35b0cae8
Artor101 <artor101@gmail.com>
2026-05-14 15:23:42 +0000

    fixed Fifo file for AuAu130

--- PHENIX_2004_I623413/RunFIFOAuAu130.sh
+++ PHENIX_2004_I623413/RunFIFOAuAu130.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#mkfifo /tmp/fifo.hepmc 
+rm *.so
+rivet-build RivetPHENIX_2004_I623413.so PHENIX_2004_I623413.cc
 
+
+date
 mkfifo AuAu130GeV.hepmc 
 cat AuAu130GeV.hepmc > /dev/null &
+
 pythia8-main144 -c ../RunPYTHIAInDocker/AuAu130GeV.cmnd -c main144HepMC.cmnd -c main144RivetAuAu130.cmnd -o AuAu130GeV
+
 pkill -f "cat AuAu130GeV.hepmc"
 rm -f AuAu130GeV.hepmc
+date
